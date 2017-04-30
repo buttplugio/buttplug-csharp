345170cd55d0b70da3fb8be1c6d92da2a60c8f30
Kyle Machulis <kyle@machul.is>
2017-04-29 21:58:42 -0700

    feat: Added device storage in ButtplugService
    ButtplugService now stores live devices with indexes. We're still
    opening all devices we can on first finding, will fix later.

--- Buttplug/Core/ButtplugService.cs
+++ Buttplug/Core/ButtplugService.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Buttplug.Messages;
 using System.Threading.Tasks;
 
 namespace Buttplug
 {
     public class DeviceAddedEventArgs : EventArgs
     {
+        public IButtplugDevice Device { get; }
         public DeviceAddedEventArgs(IButtplugDevice d)
         {
-            this.device = d;
+            this.Device = d;
         }
+    }
 
-        public IButtplugDevice device;
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        public IButtplugMessage Message { get; }
+        public MessageReceivedEventArgs(IButtplugMessage aMsg)
+        {
+            Message = aMsg;
+        }
     }
 
     abstract class IDeviceManager
@@ -31,12 +39,17 @@ namespace Buttplug
     public class ButtplugService
     {
         List<IDeviceManager> Managers;
+        Dictionary<uint, IButtplugDevice> Devices;
+        uint DeviceIndex;
         public event EventHandler<DeviceAddedEventArgs> DeviceAdded;
         // TODO Should I just make StartScanning async across device managers?
         public event EventHandler FinishedScanning;
+        public event EventHandler<MessageReceivedEventArgs> MessageReceived;
 
         public ButtplugService()
         {
+            Devices = new Dictionary<uint, IButtplugDevice>();
+            DeviceIndex = 0;
             Managers = new List<IDeviceManager>();
             Managers.Add(new BluetoothManager());
             Managers.Add(new GamepadManager());
@@ -45,7 +58,22 @@ namespace Buttplug
 
         public void DeviceAddedHandler(object o, DeviceAddedEventArgs e)
         {
+            Devices.Add(DeviceIndex, e.Device);
             DeviceAdded?.Invoke(this, e);
+        }
+
+        public bool SendMessage(IButtplugMessage aMsg)
+        {
+            switch (aMsg)
+            {
+                case IButtplugDeviceMessage m:
+                    if (!Devices.ContainsKey(m.DeviceIndex))
+                    {
+                        return false;
+                    }
+                    return Devices[m.DeviceIndex].ParseMessage(m);
+            }
+            return false;
         }
 
         public void StartScanning()
