a33cef7032080a6ad52b1b191757337c14abfeff
Kyle Machulis <kyle@machul.is>
2017-05-02 22:42:58 -0700

    chore: Replaced accidentally extracted LovenseDeviceFactory class
    VS got a little overzealous and extracted a class that I didn't want
    extracted. Adding class back to where it was.

--- Buttplug/Devices/Lovense.cs
+++ Buttplug/Devices/Lovense.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Buttplug.Messages;
 using Windows.Storage.Streams;
+using LanguageExt;
 using NLog;
 
 namespace Buttplug.Devices
@@ -13,6 +15,48 @@ namespace Buttplug.Devices
         static public readonly Guid SERVICE = new Guid("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
         static public readonly Guid TX_CHAR = new Guid("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
         static public readonly Guid RX_CHAR = new Guid("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+    }
+
+    class LovenseDeviceFactory : ButtplugBluetoothDeviceFactory
+    {
+        public LovenseDeviceFactory()
+        {
+            NameFilters.Add("LVS-S001");
+            NameFilters.Add("LVS-Z001");
+        }
+
+        async public override Task<Option<IButtplugDevice>> CreateDeviceAsync(BluetoothLEDevice aDevice)
+        {
+            GattDeviceServicesResult srvResult = await aDevice.GetGattServicesForUuidAsync(LovenseBluetoothInfo.SERVICE, BluetoothCacheMode.Cached);
+            if (srvResult.Status != GattCommunicationStatus.Success || !srvResult.Services.Any())
+            {
+                return Option<IButtplugDevice>.None;
+            }
+            var service = srvResult.Services.First();
+
+            GattCharacteristicsResult chrResult = await service.GetCharacteristicsAsync();
+            if (chrResult.Status != GattCommunicationStatus.Success)
+            {
+                return Option<IButtplugDevice>.None;
+            }
+
+            var chrs =
+                (from x in chrResult.Characteristics
+                 where x.Uuid == LovenseBluetoothInfo.TX_CHAR
+                 from y in chrResult.Characteristics
+                 where y.Uuid == LovenseBluetoothInfo.RX_CHAR
+                 select (x, y));
+
+            if (!chrs.Any())
+            {
+                return Option<IButtplugDevice>.None;
+            }
+
+            GattCharacteristic tx = null;
+            GattCharacteristic rx = null;
+            (tx, rx) = chrs.First().ToTuple();
+            return Option<IButtplugDevice>.Some(new Lovense(aDevice, tx, rx));
+        }
     }
 
     class Lovense : IButtplugDevice
