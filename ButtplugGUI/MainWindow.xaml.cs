eeaeeb288a8ac1e02e2673c768ad85296d8532f2
Kyle Machulis <kyle@machul.is>
2017-05-03 00:31:22 -0700

    feat: Added device scanning to ButtplugGUI
    Scanning will now show new devices in the device tab, with their name
    and index.

--- ButtplugGUI/MainWindow.xaml.cs
+++ ButtplugGUI/MainWindow.xaml.cs
@@ -12,17 +12,74 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Buttplug;
+using System.Collections.ObjectModel;
+using Buttplug.Messages;
 
 namespace ButtplugGUI
 {
+
+    public class Device
+    {
+        public String Name { get; }
+        public uint Index { get; }
+
+        public Device(uint aIndex, String aName)
+        {
+            Index = aIndex;
+            Name = aName;
+        }
+
+        public override string ToString()
+        {
+            return $"{Index}: {Name}";
+        }
+    }
+
+    public class DeviceList : ObservableCollection<Device>
+    {
+
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        ButtplugService BPServer;
+        DeviceList Devices;
         public MainWindow()
         {
             InitializeComponent();
+            BPServer = new ButtplugService();
+            BPServer.MessageReceived += OnMessageReceived;
+            Devices = new DeviceList();
+            DeviceListBox.ItemsSource = Devices;
+        }
+
+        public void OnMessageReceived(object o, MessageReceivedEventArgs e)
+        {
+            switch (e.Message)
+            {
+                case DeviceAddedMessage m:
+                    Devices.Add(new Device(m.DeviceIndex, m.DeviceName));
+                    break;
+            }
+        }
+
+        private void ScanButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)ScanButton.Content == "Start Scanning")
+            {
+                BPServer.StartScanning();
+                ScanButton.Content = "Stop Scanning";
+            }
+            else
+            {
+                BPServer.StopScanning();
+                ScanButton.Content = "Start Scanning";
+            }
+           
         }
     }
 }
