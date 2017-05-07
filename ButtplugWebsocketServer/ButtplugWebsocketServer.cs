e0c5c6ab9d65bad465c81926f4dac201469926b0
Kyle Machulis <kyle@machul.is>
2017-05-06 19:39:52 -0700

    chore: Move websocket server to its own assembly
    Websocket server was built into CLI. Should be in its own assembly so
    it's easy to use in the GUI too.

---
+++ ButtplugWebsocketServer/ButtplugWebsocketServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buttplug.Core;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using NLog;
+using LanguageExt;
+
+namespace ButtplugWebsocketServer
+{
+    public class ButtplugWebsocketServer : WebSocketBehavior
+    {
+        private readonly ButtplugService _buttplug;
+        private NLog.Logger _bpLogger;
+        public ButtplugWebsocketServer()
+        {
+            _bpLogger = LogManager.GetLogger("Buttplug");
+            _buttplug = new ButtplugService();
+            _buttplug.MessageReceived += OnMessageReceived;
+        }
+
+        protected override async void OnMessage(MessageEventArgs e)
+        {
+            base.OnMessage(e);
+            await _buttplug.SendMessage(e.Data);
+        }
+
+        public void OnMessageReceived(object o, MessageReceivedEventArgs e)
+        {
+            var msg = ButtplugJsonMessageParser.Serialize(e.Message);
+        }
+    }
+}
