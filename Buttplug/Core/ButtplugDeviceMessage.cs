7e5e341403de4f00a189931c5bdafb17e2ef15ca
Kyle Machulis <kyle@machul.is>
2017-05-08 21:43:35 -0700

    chore: Extract ButtplugDeviceMessage to its own file

--- Buttplug/Core/ButtplugDeviceMessage.cs
+++ Buttplug/Core/ButtplugDeviceMessage.cs
@@ -2,4 +2,14 @@
 
 namespace Buttplug.Core
 {
+    public class ButtplugDeviceMessage : ButtplugMessage
+    {
+        [JsonProperty(Required = Required.Always)]
+        public uint DeviceIndex { get; }
+
+        public ButtplugDeviceMessage(uint aId, uint aDeviceIndex) : base(aId)
+        {
+            DeviceIndex = aDeviceIndex;
+        }
+    }
 }
