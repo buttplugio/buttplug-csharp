24d1e832fb8bafbed49de607c65238887803bf9b
Kyle Machulis <kyle@machul.is>
2017-05-14 15:04:22 -0700

    feat: Finish About Dialog
    Now have about dialog pane in ButtplugGUI that shows important URLs
    and also begs for money.
    
    Fixes #24

--- ButtplugGUI/MainWindow.xaml.cs
+++ ButtplugGUI/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
 using ButtplugKiirooPlatformEmulator;
@@ -212,6 +213,16 @@ namespace ButtplugGUI
             {
                 LogManager.GetCurrentClassLogger().Error($"Log Level \"{level}\" is not a valid log level!");
             }
+        }
+
+        private void PatreonRequestNavigate(object o, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new Uri("http://patreon.com/qdot").AbsoluteUri);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
         }
     }
 }
