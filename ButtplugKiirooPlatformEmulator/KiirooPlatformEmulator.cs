c6c43dcd8eb77a03a7843f991b7ebd1233a32116
Kyle Machulis <kyle@machul.is>
2017-05-17 19:01:11 -0700

    feat: Move from uhttpsharp to HttpListener
    uhttpsharp was causing thread deadlocks due to using a version of
    LibLog that was older than what Buttplug uses. Also, we were having
    problems getting uhttpsharp to stop listening on sockets. Since the
    Kiiroo emulator is so simple, we can just use asynchronous
    HttpListener for the 2 REST verbs we need. This will need more tests
    now that we're handling it ourselves though.
    
    Fixes #42
    Fixes #39
    Affects #3

--- ButtplugKiirooPlatformEmulator/KiirooPlatformEmulator.cs
+++ ButtplugKiirooPlatformEmulator/KiirooPlatformEmulator.cs
@@ -1,64 +1,11 @@
-﻿using Buttplug.Core;
-using Buttplug.Messages;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
 using System.IO;
 using System.Net;
-using System.Net.Sockets;
-using System.Threading.Tasks;
-using uhttpsharp;
-using uhttpsharp.Handlers;
-using uhttpsharp.Listeners;
-using uhttpsharp.RequestProviders;
+using System.Text;
+using System.Web;
 
 namespace ButtplugKiirooPlatformEmulator
 {
-
-    public class SendStatusHandler : IHttpRequestHandler
-    {
-        public Task Handle(IHttpContext aContext, Func<Task> aNext)
-        {
-            var memoryStream = new MemoryStream();
-            var w = new StreamWriter(memoryStream);
-            w.Write("{\"connectedDeviceName\":\"ONYX\",\"bluetoothAddress\":\"8CDE52B866B5\",\"firmwareUpdateProgres\":0,\"remoteDevice\":\"not connected\",\"devicestatus\":\"NORMAL\",\"localDevice\":\"connected\",\"previousdevice_connectionurl\":\"btspp://8CDE52B866B5:1;authenticate=false;encrypt=false;master=false\",\"readOnlyMode\":false,\"streamToDeviceEnabled\":true,\"delay\":0,\"writeOnlyMode\":false,\"currentFW\":\"91\",\"waitingforusbcable\":true,\"bluetoothOn\":true,\"previousdevice_name\":\"ONYX\",\"uienabled\":true,\"newFWVersionAvailable\":false,\"previousdevice_bluetoothaddress\":\"8CDE52B866B5\",\"statusCode\":1}");
-            w.Flush();
-            var h = new Dictionary<string, string> { { "Access-Control-Allow-Origin", "*" } };
-            aContext.Response = new HttpResponse(HttpResponseCode.Ok, "application/json; charset=utf-8", memoryStream, true, h);
-            return Task.Factory.GetCompleted();
-        }
-    }
-
-    public class SendDataHandler : IHttpRequestHandler
-    {
-        private EventHandler<KiirooPlatformEventArgs> _handler;
-
-        public SendDataHandler(EventHandler<KiirooPlatformEventArgs> aHandler)
-        {
-            _handler = aHandler;
-        }
-
-        public async Task Handle(IHttpContext aContext, Func<Task> aNext)
-        {
-            var memoryStream = new MemoryStream();
-            var w = new StreamWriter(memoryStream);
-            w.Write("{}");
-            w.Flush();
-            var h = new Dictionary<string, string> { { "Access-Control-Allow-Origin", "*" } };
-            aContext.Response = new HttpResponse(HttpResponseCode.Ok, "application/json; charset=utf-8", memoryStream, true, h);
-            try
-            {
-                var position = ushort.Parse(aContext.Request.Post.Parsed.GetByName("data"));
-                _handler?.Invoke(this, new KiirooPlatformEventArgs(position));
-            }
-            catch (FormatException)
-            {
-                // Swallow format exceptions, as sometimes scripts can send "undefined". 
-            }
-            await Task.Factory.GetCompleted();
-        }
-    }
-
     public class KiirooPlatformEventArgs : EventArgs
     {
         public ushort Position { get; private set; }
@@ -69,34 +16,131 @@ namespace ButtplugKiirooPlatformEmulator
         }
     }
 
-
     public class KiirooPlatformEmulator
     {
-        private HttpServer _httpServer;
-        private TcpListener _tcpAdapter;
+        private readonly HttpListener _httpListener;
+        private bool _stop;
+        private bool _isRunning;
+
         public event EventHandler<KiirooPlatformEventArgs> OnKiirooPlatformEvent;
 
         public KiirooPlatformEmulator()
         {
-            _tcpAdapter = new TcpListener(IPAddress.Loopback, 6969);
-            _httpServer = new HttpServer(new HttpRequestProvider());
-            // Normal port 80 :
-            _httpServer.Use(new TcpListenerAdapter(_tcpAdapter));
-
-            // Handler classes :
-            _httpServer.Use(new HttpRouter()
-                .With("senddata", new SendDataHandler(OnKiirooPlatformEvent))
-                .With("status", new SendStatusHandler()));
+            _httpListener = new HttpListener();
+            _httpListener.Prefixes.Add("http://localhost:6969/");
+            _stop = false;
+            _isRunning = false;
         }
 
-        public void StartServer()
+        // http://stackoverflow.com/questions/5197579/getting-form-data-from-httplistenerrequest
+        public static string GetRequestPostData(HttpListenerRequest request)
         {
-            _httpServer.Start();
+            if (!request.HasEntityBody)
+            {
+                return null;
+            }
+            using (var body = request.InputStream)
+            {
+                using (var reader = new StreamReader(body, request.ContentEncoding))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public async void StartServer()
+        {
+            if (_isRunning)
+            {
+                return;
+            }
+            _isRunning = true;
+            _stop = false;
+            _httpListener.Start();
+            while (!_stop)
+            {
+                HttpListenerContext ctx = null;
+                try
+                {
+                    ctx = await _httpListener.GetContextAsync();
+                }
+                catch (HttpListenerException ex)
+                {
+                    if (ex.ErrorCode == 995)
+                    {
+                        StopServer();
+                        _isRunning = false;
+                        return;
+                    }
+                }
+
+                if (ctx == null) continue;
+
+                //got a request
+                var response = ctx.Response;
+                response.Headers.Add("Access-Control-Allow-Origin", "*");
+                response.Headers.Add(HttpResponseHeader.CacheControl, "private, no-store");
+                response.ContentType = "application/json; charset=utf-8";
+                response.StatusCode = (int)HttpStatusCode.OK;
+                if (ctx.Request.Url.Segments.Length < 2)
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Close();
+                    continue;
+                }
+                var methodName = ctx.Request.Url.Segments[1].Replace("/", "");
+                var json = "";
+                switch (methodName)
+                {
+                    case "status":
+                        json = "{\"connectedDeviceName\":\"ONYX\",\"bluetoothAddress\":\"8CDE52B866B5\",\"firmwareUpdateProgres\":0,\"remoteDevice\":\"not connected\",\"devicestatus\":\"NORMAL\",\"localDevice\":\"connected\",\"previousdevice_connectionurl\":\"btspp://8CDE52B866B5:1;authenticate=false;encrypt=false;master=false\",\"readOnlyMode\":false,\"streamToDeviceEnabled\":true,\"delay\":0,\"writeOnlyMode\":false,\"currentFW\":\"91\",\"waitingforusbcable\":true,\"bluetoothOn\":true,\"previousdevice_name\":\"ONYX\",\"uienabled\":true,\"newFWVersionAvailable\":false,\"previousdevice_bluetoothaddress\":\"8CDE52B866B5\",\"statusCode\":1}";
+                        break;
+                    case "senddata":
+                        json = "{}";
+                        var post = GetRequestPostData(ctx.Request);
+                        var data = HttpUtility.ParseQueryString(post);
+                        if (!data.HasKeys())
+                        {
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
+                            response.Close();
+                            continue;
+                        }
+                        try
+                        {
+                            var position = ushort.Parse(data["data"]);
+                            OnKiirooPlatformEvent?.Invoke(this, new KiirooPlatformEventArgs(position));
+                        }
+                        catch (FormatException)
+                        {
+                            // Swallow format exceptions, as sometimes scripts can send "undefined".
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
+                            response.Close();
+                            continue;
+                        }
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        response.Close();
+                        continue;
+                }
+
+                var messageBytes = Encoding.UTF8.GetBytes(json);
+                response.ContentLength64 = messageBytes.Length;
+                await response.OutputStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+                response.OutputStream.Close();
+                response.Close();
+            }
+            _isRunning = false;
         }
 
         public void StopServer()
         {
-            _httpServer.Dispose();
+            if (!_isRunning)
+            {
+                return;
+            }
+            _stop = true;
+            _httpListener.Stop();
         }
     }
 }
