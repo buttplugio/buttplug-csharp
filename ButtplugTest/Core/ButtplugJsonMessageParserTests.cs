598724656c491d46e010c4a46967761888a10bcd
blackspherefollower <darktech@black-sphere.co.uk>
2017-06-09 19:56:44 +0100

    Supporting multiple messages in a single JSON array
    The schema allows multiple messages to be sent in a single JSON array, but the C# server only handled the first message.

--- ButtplugTest/Core/ButtplugJsonMessageParserTests.cs
+++ ButtplugTest/Core/ButtplugJsonMessageParserTests.cs
@@ -9,11 +9,16 @@ namespace ButtplugTest.Core
         [Fact]
         public void JsonConversionTest()
         {
-            var m = new Buttplug.Messages.Test("ThisIsATest", ButtplugConsts.SYSTEM_MSG_ID);
+            var m1 = new Buttplug.Messages.Test("ThisIsATest", ButtplugConsts.SYSTEM_MSG_ID);
+            var m2 = new Buttplug.Messages.Test("ThisIsAnotherTest", ButtplugConsts.SYSTEM_MSG_ID);
             var logger = new ButtplugLogManager();
-            var msg = new ButtplugJsonMessageParser(logger).Serialize(m);
+            var msg = new ButtplugJsonMessageParser(logger).Serialize(m1);
             Assert.True(msg.Length > 0);
             Assert.Equal("[{\"Test\":{\"TestString\":\"ThisIsATest\",\"Id\":0}}]", msg);
+            ButtplugMessage[] msgs = { m1, m2 };
+            msg = new ButtplugJsonMessageParser(logger).Serialize(msgs);
+            Assert.True(msg.Length > 0);
+            Assert.Equal("[{\"Test\":{\"TestString\":\"ThisIsATest\",\"Id\":0}},{\"Test\":{\"TestString\":\"ThisIsAnotherTest\",\"Id\":0}}]", msg);
         }
 
         // Not valid JSON
@@ -23,20 +28,22 @@ namespace ButtplugTest.Core
         // Valid json but not an object
         [InlineData("[]")]
         // Not a message type
-        [InlineData("{\"NotAMessage\":{}}")]
+        [InlineData("[{\"NotAMessage\":{}}]")]
         // Valid json and message type but not in correct format
-        [InlineData("{\"Test\":[]}")]
+        [InlineData("[{\"Test\":[]}]")]
         // Valid json and message type but not in correct format
-        [InlineData("{\"Test\":{}}")]
+        [InlineData("[{\"Test\":{}}]")]
         // Valid json and message type but with erroneous content
-        [InlineData("{\"Test\":{\"TestString\":\"Error\",\"Id\":0}}")]
+        [InlineData("[{\"Test\":{\"TestString\":\"Error\",\"Id\":0}}]")]
         // Valid json and message type but with extra content
-        [InlineData("{\"Test\":{\"TestString\":\"Yup\",\"NotAField\":\"NotAValue\",\"Id\":0}}")]
+        [InlineData("[{\"Test\":{\"TestString\":\"Yup\",\"NotAField\":\"NotAValue\",\"Id\":0}}]")]
         [Theory]
         public void DeserializeIncorrectMessages(string x)
         {
             var p = new ButtplugJsonMessageParser(new ButtplugLogManager());
-            Assert.True(p.Deserialize(x) is Error);
+            var res = p.Deserialize(x);
+            Assert.True(res.Length == 1);
+            Assert.True(res[0] is Error);
         }
 
         [Fact]
@@ -44,7 +51,8 @@ namespace ButtplugTest.Core
         {
             var p = new ButtplugJsonMessageParser(new ButtplugLogManager());
             var m = p.Deserialize("[{\"Test\":{\"TestString\":\"Test\",\"Id\":0}}]");
-            switch (m)
+            Assert.True(m.Length == 1);
+            switch (m[0])
             {
                 case Error e:
                     Assert.True(false, $"Got Error: {e.ErrorMessage}");
