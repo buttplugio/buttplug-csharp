d4113ea1a556a472fc07d8c99787207c0004374f
blackspherefollower <darktech@black-sphere.co.uk>
2017-06-22 13:52:39 +0100

    Adding HTML landing page on /index.html
    The web socket library can be used as a HttpServer too.
    This will give us a test page we can point browsers to to test SSL issues etc.
    
    In the future, it'd be cool to present syncydink here, so that users can test with just a browser (no additional media required).

--- ButtplugWebsockets/ButtplugWebsocketServer.cs
+++ ButtplugWebsockets/ButtplugWebsocketServer.cs
@@ -4,30 +4,57 @@ using Buttplug.Core;
 using JetBrains.Annotations;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using WebSocketSharp.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ButtplugWebsockets
 {
     public class ButtplugWebsocketServer
     {
         [NotNull]
-        private WebSocketServer _wsServer;
+        private HttpServer _wsServer;
 
         public ButtplugWebsocketServer()
         {
-            _wsServer = new WebSocketServer(12345);
+            _wsServer = new HttpServer(12345);
             _wsServer.RemoveWebSocketService("/buttplug");
+            _wsServer.OnGet += OnGetHandler;
         }
 
         public void StartServer([NotNull] ButtplugService aService)
         {
-            _wsServer.WebSocketServices.AddService<ButtplugWebsocketServerBehavior>("/buttplug", (obj) => obj.Service = aService );
+            _wsServer.WebSocketServices.AddService<ButtplugWebsocketServerBehavior>("/buttplug", (obj) => obj.Service = aService);
             _wsServer.Start();
         }
 
         public void StopServer()
         {
             _wsServer.Stop();
-            _wsServer.RemoveWebSocketService("/buttplug");            
+            _wsServer.RemoveWebSocketService("/buttplug");
+        }
+
+        protected void OnGetHandler(object sender, HttpRequestEventArgs e)
+        {
+            var req = e.Request;
+            var res = e.Response;
+
+            // Wouldn't it be cool to present syncydink here?
+
+            var path = req.RawUrl;
+            if (path == "/")
+                path += "index.html";
+
+            if (path != "/index.html")
+            {
+                res.StatusCode = (int)HttpStatusCode.TemporaryRedirect;
+                res.RedirectLocation = "/index.html";
+                return;
+            }
+
+            res.ContentType = "text/html";
+            res.ContentEncoding = Encoding.UTF8;
+            res.WriteContent(Encoding.UTF8.GetBytes("<html><head><title>Buttplug server</title></head><body><h1>Buttplug server</h1><p>The server is running.</p></body></html>"));
+
         }
     }
 
