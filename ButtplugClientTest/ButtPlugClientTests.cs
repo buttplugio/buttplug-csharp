f609e7f0099d5c4e6a8ebbf323f2ab489bf483b6
blackspherefollower <darktech@black-sphere.co.uk>
2017-07-06 07:44:48 +0100

    Adding message response handling and the ping timer to the C# client.

--- ButtplugClientTest/ButtPlugClientTests.cs
+++ ButtplugClientTest/ButtPlugClientTests.cs
@@ -3,6 +3,7 @@ using Buttplug.Messages;
 using ButtplugClient.Core;
 using ButtplugWebsockets;
 using System;
+using System.Threading;
 using Xunit;
 
 namespace ButtplugClientTest
@@ -19,15 +20,26 @@ namespace ButtplugClientTest
         {
             var server = new ButtplugWebsocketServer();
             server.StartServer(this);
-            
 
             var client = new ButtplugWSClient("Test client");
-            client.Connect(new Uri("ws://localhost:12345/buttplug")).GetAwaiter().GetResult();
-            var res = client.SendMessage(new Test("Test string", 6)).GetAwaiter().GetResult();
-            Assert.True(res.Length == 1);
-            Assert.True(res[0] is Test);
-            Assert.True(((Test)res[0]).TestString == "Test string");
-            Assert.True(((Test)res[0]).Id == 6);
+            client.Connect(new Uri("ws://localhost:12345/buttplug")).Wait();
+
+            var msgId = client.nextMsgId;
+            var res = client.SendMessage(new Test("Test string", msgId)).GetAwaiter().GetResult();
+            Assert.True(res != null);
+            Assert.True(res is Test);
+            Assert.True(((Test)res).TestString == "Test string");
+            Assert.True(((Test)res).Id == msgId);
+
+            // Check ping is working
+            Thread.Sleep(200);
+
+            msgId = client.nextMsgId;
+            res = client.SendMessage(new Test("Test string", msgId)).GetAwaiter().GetResult();
+            Assert.True(res != null);
+            Assert.True(res is Test);
+            Assert.True(((Test)res).TestString == "Test string");
+            Assert.True(((Test)res).Id == msgId);
         }
 
     }
