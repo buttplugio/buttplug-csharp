682d7397e14827210b0d948e4e71c07a4047035c
Kyle Machulis <kyle@machul.is>
2017-07-09 16:16:49 -0700

    fix: Fix application root namespaces
    Affects #157

--- Buttplug.Apps.KiirooEmulatorGUI/Properties/Resources.Designer.cs
+++ Buttplug.Apps.KiirooEmulatorGUI/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Buttplug.Apps.KiirooEmulatorGUI.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ButtplugKiirooEmulatorGUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Buttplug.Apps.KiirooEmulatorGUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
