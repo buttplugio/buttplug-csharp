8006ffc9f0d5669873e96a2e9961bf6fd592db8f
Kyle Machulis <kyle@machul.is>
2017-08-12 13:57:49 -0700

    feat: Created Buttplug.Apps.XInputInjector.GUI
    Added GUI for XInput injection. Lists processes, allows user to attach
    to a process and scan for/select devices. Outputs average of motors to
    a SingleMotorVibrateCmd.

---
+++ Buttplug.Apps.XInputInjector.GUI/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using Buttplug.Apps.XInputInjector.Interface;
+using Buttplug.Apps.XInputInjector.Payload;
+using Buttplug.Components.Controls;
+using Buttplug.Core.Messages;
+using Buttplug.Server;
+using EasyHook;
+using JetBrains.Annotations;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Ipc;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Buttplug.Apps.XInputInjector.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        [NotNull]
+        private readonly ButtplugServer _bpServer;
+
+        [NotNull]
+        private readonly Logger _log;
+
+        [NotNull]
+        private readonly ProcessTab _processTab;
+
+        private IpcServerChannel _xinputHookServer;
+        private string _channelName;
+        private List<ButtplugDeviceInfo> _devices = new List<ButtplugDeviceInfo>();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Application.Current == null)
+            {
+                return;
+            }
+            ButtplugTab.GetLogControl().MaxLogs = 10000;
+
+            ButtplugTab.SetServerDetails("XInput Hijack Server", 0);
+            _bpServer = ButtplugTab.GetServer();
+            _log = LogManager.GetCurrentClassLogger();
+            ButtplugXInputInjectorInterface.VibrationCommandReceived += OnVibrationCommand;
+            ButtplugXInputInjectorInterface.VibrationPingMessageReceived += OnVibrationPingMessage;
+            ButtplugXInputInjectorInterface.VibrationExceptionReceived += OnVibrationException;
+            ButtplugXInputInjectorInterface.VibrationExitReceived += OnVibrationExit;
+            Task.FromResult(_bpServer.SendMessage(new RequestServerInfo("Buttplug XInput Injector")));
+            _processTab = new ProcessTab();
+            _processTab.ProcessAttachRequested += OnAttachRequested;
+            _processTab.ProcessDetachRequested += OnDetachRequested;
+            ButtplugTab.SetApplicationTab("Processes", _processTab);
+            ButtplugTab.AddDevicePanel(_bpServer);
+            ButtplugTab.SelectedDevicesChanged += OnSelectedDevicesChanged;
+        }
+
+        private void OnSelectedDevicesChanged(object aObj, List<ButtplugDeviceInfo> aDevices)
+        {
+            _devices = aDevices;
+        }
+
+        private void OnVibrationException(object aObj, Exception aEx)
+        {
+            _log.Error($"Remote Exception: {aEx}");
+            Dispatcher.Invoke(() =>
+            {
+                OnDetachRequested(this, true);
+                _processTab.ProcessError = "Error attaching, see logs for details.";
+            });
+        }
+
+        private void OnVibrationPingMessage(object aObj, string aMsg)
+        {
+            _log.Info($"Remote Ping Message: {aMsg}");
+        }
+
+        private void OnVibrationExit(object aObj, bool aTrue)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                Detach();
+                _processTab.ProcessError = "Attached process detached or exited";
+            });
+        }
+
+        private void Detach()
+        {
+            ButtplugXInputInjectorInterface.Detach();
+            _processTab.Attached = false;
+            _channelName = null;
+            _xinputHookServer = null;
+        }
+
+        private async void OnVibrationCommand(object aObj, Vibration aVibration)
+        {
+            await Dispatcher.Invoke(async () =>
+            {
+                foreach (var device in _devices)
+                {
+                    // For now, we only handle devices that can take vibration messages.
+                    if (!device.Messages.Contains(typeof(SingleMotorVibrateCmd).Name))
+                    {
+                        continue;
+                    }
+                    await _bpServer.SendMessage(new SingleMotorVibrateCmd(device.Index,
+                        (aVibration.LeftMotorSpeed + aVibration.RightMotorSpeed) / (2.0 * 65535.0)));
+                }
+            });
+        }
+
+        private void OnAttachRequested(object aObject, int aProcessId)
+        {
+            try
+            {
+                _xinputHookServer = RemoteHooking.IpcCreateServer<ButtplugXInputInjectorInterface>(
+                    ref _channelName,
+                    WellKnownObjectMode.Singleton);
+                _log.Info($"Beginning process injection on {aProcessId}...");
+                RemoteHooking.Inject(
+                    aProcessId,
+                    InjectionOptions.Default,
+                    System.IO.Path.Combine(
+                        System.IO.Path.GetDirectoryName(typeof(ButtplugXInputInjectorPayload).Assembly.Location),
+                        "ButtplugXInputInjectorPayload.dll"),
+                    System.IO.Path.Combine(
+                        System.IO.Path.GetDirectoryName(typeof(ButtplugXInputInjectorPayload).Assembly.Location),
+                        "ButtplugXInputInjectorPayload.dll"),
+                    // the optional parameter list...
+                    _channelName);
+                _log.Info($"Finished process injection on {aProcessId}...");
+                _processTab.Attached = true;
+                _processTab.ProcessError = "Attached to process";
+            }
+            catch (Exception ex)
+            {
+                Detach();
+                _log.Error(ex);
+                _processTab.ProcessError = "Error attaching, see logs for details.";
+            }
+        }
+
+        private void OnDetachRequested(object aObject, bool aShouldDetach)
+        {
+            Detach();
+        }
+    }
+}
---
+++ Buttplug.Apps.XInputInjector.Payload/ButtplugXInputInjectorPayload.cs
@@ -0,0 +1,121 @@
+﻿using EasyHook;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Buttplug.Apps.XInputInjector.Interface;
+
+namespace Buttplug.Apps.XInputInjector.Payload
+{
+    public class ButtplugXInputInjectorPayload : IEntryPoint
+    {
+        private readonly ButtplugXInputInjectorInterface _interface;
+        private LocalHook _xinputSetStateHookObj;
+        private Vibration _lastMessage = new Vibration {LeftMotorSpeed = 65535, RightMotorSpeed = 65535};
+        private readonly Queue<Vibration> _messageQueue = new Queue<Vibration>();
+        private static Exception _ex;
+        private static ButtplugXInputInjectorPayload _instance;
+
+        public ButtplugXInputInjectorPayload(
+            RemoteHooking.IContext aInContext,
+            String aInChannelName)
+        {
+            _interface = RemoteHooking.IpcConnectClient<ButtplugXInputInjectorInterface>(aInChannelName);
+            _instance = this;
+        }
+
+        public void Run(
+            RemoteHooking.IContext aInContext,
+            String aInArg1)
+        {
+            _interface.Ping(RemoteHooking.GetCurrentProcessId(), "Payload installed. Running payload loop.");
+            try
+            {
+                _xinputSetStateHookObj = LocalHook.Create(
+                    LocalHook.GetProcAddress("xinput1_3.dll", "XInputSetState"),
+                    new XInputSetStateDelegate(XInputSetStateHookFunc),
+                    null);
+
+                // Set hook for all threads.
+                _xinputSetStateHookObj.ThreadACL.SetExclusiveACL(new Int32[1]);
+            }
+            catch (Exception e)
+            {
+                _interface.ReportError(RemoteHooking.GetCurrentProcessId(), e);
+                return;
+            }
+
+            try
+            {
+                while (_interface.Ping(RemoteHooking.GetCurrentProcessId(), ""))
+                {
+                    Thread.Sleep(1);
+
+                    if (_messageQueue.Count > 0)
+                    {
+                        lock (_messageQueue)
+                        {
+                            _interface.Report(RemoteHooking.GetCurrentProcessId(), _messageQueue);
+                            _messageQueue.Clear();
+                        }
+                    }
+                    if (_ex != null)
+                    {
+                        _interface.ReportError(RemoteHooking.GetCurrentProcessId(), _ex);
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _interface.ReportError(RemoteHooking.GetCurrentProcessId(), e);
+            }
+            _interface.Ping(RemoteHooking.GetCurrentProcessId(), "Exiting payload loop");
+            _interface.Exit();
+        }
+
+        [DllImport("xinput1_3.dll", CallingConvention = CallingConvention.StdCall, EntryPoint = "XInputSetState")]
+        private static extern unsafe int XInputSetState(int arg0, void* arg1);
+
+        private static unsafe int XInputSetStateShim(int aUserIndex, Vibration aVibrationRef)
+        {
+            return XInputSetState(aUserIndex, &aVibrationRef);
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, SetLastError = true)]
+        delegate uint XInputSetStateDelegate(int aGamePadIndex, ref Vibration aVibrationRef);
+
+        private static uint XInputSetStateHookFunc(int aGamePadIndex, ref Vibration aVibrationRef)
+        {
+            try
+            {
+                // Always send to the controller first, then do what we need to.
+                XInputSetStateShim(aGamePadIndex, aVibrationRef);
+
+                ButtplugXInputInjectorPayload This = _instance;
+                // No reason to send duplicate packets.
+                if (This._lastMessage.LeftMotorSpeed == aVibrationRef.LeftMotorSpeed &&
+                    This._lastMessage.RightMotorSpeed == aVibrationRef.RightMotorSpeed)
+                {
+                    return 0;
+                }
+                This._lastMessage = new Vibration {
+                    LeftMotorSpeed = aVibrationRef.LeftMotorSpeed,
+                    RightMotorSpeed = aVibrationRef.RightMotorSpeed
+                };
+
+                lock (This._messageQueue)
+                {
+                    This._messageQueue.Enqueue(This._lastMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                _ex = e;
+            }
+
+            return 0;
+        }
+    }
+
+}
