c0d21db6ee167d02e83d92030298e67a48578f48
Kyle Machulis <kyle@machul.is>
2017-08-24 18:17:04 -0700

    feat: Add Lovense Domi Support
    Just needs a new set of service/characteristic IDs in the Lovense
    class. We really gotta clean up our groupings on this. :/
    
    Fixes #251

--- Buttplug.Server/Bluetooth/BluetoothSubtypeManager.cs
+++ Buttplug.Server/Bluetooth/BluetoothSubtypeManager.cs
@@ -25,6 +25,7 @@ namespace Buttplug.Server.Bluetooth
                 new LovenseRev1BluetoothInfo(),
                 new LovenseRev2BluetoothInfo(),
                 new LovenseRev3BluetoothInfo(),
+                new LovenseRev4BluetoothInfo(),
                 new MagicMotionBluetoothInfo(),
                 new VibratissimoBluetoothInfo(),
                 new VorzeA10CycloneInfo(),
