1b67cf215086f2b80bc781be30b3b40dabe4825b
Orgasmscience <orgasmscience@gmail.com>
2017-09-18 19:40:46 +0200

    Added support for Youcups Warrior II masturbator

---
+++ Buttplug.Server/Bluetooth/Devices/Youcups.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Buttplug.Core;
+using Buttplug.Core.Messages;
+
+namespace Buttplug.Server.Bluetooth.Devices
+{
+    internal class YoucupsBluetoothInfo : IBluetoothDeviceInfo
+    {
+        public enum Chrs : uint
+        {
+            Tx = 0,
+            Rx,
+        }
+
+        public Guid[] Services { get; } = { new Guid("0000fee9-0000-1000-8000-00805f9b34fb") };
+
+        public string[] Names { get; } =
+        {
+            // Warrior II
+            "Youcups",
+        };
+
+        public Guid[] Characteristics { get; } =
+        {
+            // tx characteristic
+            new Guid("d44bc439-abfd-45a2-b575-925416129600"), // ,
+
+            // rx characteristic
+        };
+
+        public IButtplugDevice CreateDevice(IButtplugLogManager aLogManager,
+            IBluetoothDeviceInterface aInterface)
+        {
+            return new Youcups(aLogManager, aInterface, this);
+        }
+    }
+
+    internal class Youcups : ButtplugBluetoothDevice
+    {
+        private static Dictionary<string, string> friendlyNames = new Dictionary<string, string>()
+        {
+            { "Youcups", "Warrior II" },
+        };
+
+        public Youcups(IButtplugLogManager aLogManager,
+                       IBluetoothDeviceInterface aInterface,
+                       IBluetoothDeviceInfo aInfo)
+            : base(aLogManager,
+                   $"Youcups Device ({friendlyNames[aInterface.Name]})",
+                   aInterface,
+                   aInfo)
+        {
+            MsgFuncs.Add(typeof(SingleMotorVibrateCmd), HandleSingleMotorVibrateCmd);
+            MsgFuncs.Add(typeof(StopDeviceCmd), HandleStopDeviceCmd);
+        }
+
+        private async Task<ButtplugMessage> HandleStopDeviceCmd(ButtplugDeviceMessage aMsg)
+        {
+            BpLogger.Debug("Stopping Device " + Name);
+            return await HandleSingleMotorVibrateCmd(new SingleMotorVibrateCmd(aMsg.DeviceIndex, 0, aMsg.Id));
+        }
+
+        private async Task<ButtplugMessage> HandleSingleMotorVibrateCmd(ButtplugDeviceMessage aMsg)
+        {
+            var cmdMsg = aMsg as SingleMotorVibrateCmd;
+            if (cmdMsg is null)
+            {
+                return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
+            }
+
+            return await Interface.WriteValue(aMsg.Id,
+                Info.Characteristics[(uint)YoucupsBluetoothInfo.Chrs.Tx],
+                Encoding.ASCII.GetBytes($"$SYS,{(int)(cmdMsg.Speed * 8), 1}?"));
+        }
+    }
+}
