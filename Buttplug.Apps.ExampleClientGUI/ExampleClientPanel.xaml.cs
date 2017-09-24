3b79c60e6f6053503cd52e23855da7cbf09605bd
blackspherefollower <darktech@black-sphere.co.uk>
2017-09-24 18:28:53 +0100

    First cut at an example client

---
+++ Buttplug.Apps.ExampleClientGUI/ExampleClientPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Buttplug.Apps.ExampleClientGUI
+{
+    public partial class ClientTestPanel
+    {
+        public event EventHandler<bool> ServerStatusChanged;
+
+        public ClientTestPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void StartServer()
+        {
+            ServerStatusChanged?.Invoke(this, true);
+            ServerButton.Content = "Stop Server";
+        }
+
+        private async void StopServer()
+        {
+            ServerStatusChanged?.Invoke(this, false);
+            await Dispatcher.InvokeAsync(() =>
+            {
+                ServerButton.Content = "Start Server";
+            });
+        }
+
+        private void ServerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((string)ServerButton.Content == "Start Server")
+            {
+                StartServer();
+            }
+            else
+            {
+                StopServer();
+            }
+        }
+    }
+}
---
+++ Buttplug.Apps.ExampleClientGUI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Buttplug.Apps.ExampleClientGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Application.Current == null)
+            {
+                return;
+            }
+
+            /*ButtplugTab.SetServerDetails("Kiiroo Emulator", 0);
+            InitializeComponent();
+            var emu = new ClientTestPanel();
+            ButtplugTab.SetApplicationTab("Kiiroo Emulator", emu);*/
+        }
+    }
+}
