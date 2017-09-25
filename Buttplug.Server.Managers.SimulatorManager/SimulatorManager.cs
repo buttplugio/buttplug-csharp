2bc477e655303feca45bf94e31e6f8fe56af35a8
blackspherefollower <darktech@black-sphere.co.uk>
2017-09-25 13:14:56 -0700

    First cut simulator GUI and manager
    I'd like to use IPC to link the too, but I didn't bring appropriate documentation on the plane.

---
+++ Buttplug.Server.Managers.SimulatorManager/SimulatorManager.cs
@@ -0,0 +1,57 @@
+﻿
+using Buttplug.Core;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buttplug.Server.Managers.SimulatorManager
+{
+    public class SimulatorManager : DeviceSubtypeManager
+    {
+        private TcpListener _socket;
+
+        private Task _readThread;
+
+        private CancellationTokenSource _tokenSource;
+
+        public SimulatorManager(IButtplugLogManager aLogManager)
+            : base(aLogManager)
+        {
+            BpLogger.Info("Loading Simulator Manager");
+            _socket = new TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), 54321);
+            _socket.Start();
+
+            _tokenSource = new CancellationTokenSource();
+            _readThread = new Task(() => { connAccepter(_tokenSource.Token); }, _tokenSource.Token, TaskCreationOptions.LongRunning);
+            _readThread.Start();
+        }
+
+        private void connAccepter(CancellationToken aCancellationToken)
+        {
+            if (_socket.Pending())
+            {
+                var client = _socket.AcceptTcpClient();
+            }
+            else
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        public override void StartScanning()
+        {
+            BpLogger.Info("SimulatorManager start scanning");
+        }
+
+        public override void StopScanning()
+        {
+            BpLogger.Info("SimulatorManager stop scanning");
+        }
+
+        public override bool IsScanning()
+        {
+            return false;
+        }
+    }
+}
