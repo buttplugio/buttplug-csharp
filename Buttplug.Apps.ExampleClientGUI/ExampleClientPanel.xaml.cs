1a3c58eb89a3fa10e059fe8add2ed106820bf4ce
blackspherefollower <darktech@black-sphere.co.uk>
2017-09-25 10:20:17 -0700

    Making the example client useful
    It can now send commands of the appropriate classes
    to the server. This "should" only send to the devices
    selected on the devices tab. I can't test right now due to being 11km above Greenland.

--- Buttplug.Apps.ExampleClientGUI/ExampleClientPanel.xaml.cs
+++ Buttplug.Apps.ExampleClientGUI/ExampleClientPanel.xaml.cs
@@ -1,42 +1,98 @@
-﻿using System;
+﻿using Buttplug.Client;
+using Buttplug.Components.Controls;
+using Buttplug.Core.Messages;
+using System;
+using System.Collections.Concurrent;
 using System.Windows;
 using System.Windows.Threading;
 
 namespace Buttplug.Apps.ExampleClientGUI
 {
-    public partial class ClientTestPanel
+    public partial class ExampleClientPanel
     {
-        public event EventHandler<bool> ServerStatusChanged;
+        public ConcurrentDictionary<uint, ButtplugClientDevice> Devices = new ConcurrentDictionary<uint, ButtplugClientDevice>();
 
-        public ClientTestPanel()
+        private ButtplugWSClient _client;
+
+        public ExampleClientPanel()
         {
             InitializeComponent();
+            _client = new ButtplugWSClient("Example Client");
         }
 
-        private void StartServer()
+        private void ConnToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            ServerStatusChanged?.Invoke(this, true);
-            ServerButton.Content = "Stop Server";
-        }
-
-        private async void StopServer()
-        {
-            ServerStatusChanged?.Invoke(this, false);
-            await Dispatcher.InvokeAsync(() =>
+            if (_client.IsConnected)
             {
-                ServerButton.Content = "Start Server";
-            });
-        }
-
-        private void ServerButton_Click(object sender, RoutedEventArgs e)
-        {
-            if ((string)ServerButton.Content == "Start Server")
-            {
-                StartServer();
+                _client.Disconnect().Wait();
+                ConnToggleButton.Content = "Connect";
+                AdressTextBox.IsEnabled = true;
             }
             else
             {
-                StopServer();
+                ConnToggleButton.Content = "Disconnect";
+                AdressTextBox.IsEnabled = false;
+                _client.Connect(new Uri(AdressTextBox.Text));
+            }
+        }
+
+        private void SendLinear_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_client.IsConnected)
+            {
+                return;
+            }
+
+            foreach (var dev in Devices.Values)
+            {
+                if (dev.AllowedMessages.Contains("FleshlightLaunchFW12Cmd"))
+                {
+                    _client.SendDeviceMessage(dev,
+                        new FleshlightLaunchFW12Cmd(dev.Index,
+                            Convert.ToUInt32(LinearSpeed.Value),
+                            Convert.ToUInt32(LinearPosition.Value),
+                            _client.nextMsgId));
+                }
+            }
+        }
+
+        private void SendVibrate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_client.IsConnected)
+            {
+                return;
+            }
+
+            foreach (var dev in Devices.Values)
+            {
+                if (dev.AllowedMessages.Contains("SingleMotorVibrateCmd"))
+                {
+                    _client.SendDeviceMessage(dev,
+                        new SingleMotorVibrateCmd(dev.Index,
+                            Convert.ToDouble(VibrateSpeed.Value),
+                            _client.nextMsgId));
+                }
+            }
+        }
+
+        private void SendRotate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_client.IsConnected)
+            {
+                return;
+            }
+
+            foreach (var dev in Devices.Values)
+            {
+                if (dev.AllowedMessages.Contains("VorzeA10CycloneCmd"))
+                {
+                    bool clockwise = RotateSpeed.Value > 0;
+                    _client.SendDeviceMessage(dev,
+                        new VorzeA10CycloneCmd(dev.Index,
+                            Convert.ToUInt32(RotateSpeed.Value * (clockwise ? 1 : -1)),
+                            clockwise,
+                            _client.nextMsgId));
+                }
             }
         }
     }
--- Buttplug.Apps.ExampleClientGUI/MainWindow.xaml.cs
+++ Buttplug.Apps.ExampleClientGUI/MainWindow.xaml.cs
@@ -1,12 +1,17 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using Buttplug.Components.Controls;
+using Buttplug.Client;
 
 namespace Buttplug.Apps.ExampleClientGUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private ExampleClientPanel _clientPanel = new ExampleClientPanel();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,10 +20,33 @@ namespace Buttplug.Apps.ExampleClientGUI
                 return;
             }
 
-            /*ButtplugTab.SetServerDetails("Kiiroo Emulator", 0);
-            InitializeComponent();
-            var emu = new ClientTestPanel();
-            ButtplugTab.SetApplicationTab("Kiiroo Emulator", emu);*/
+            ButtplugTab.SetServerDetails("Buttplug Client", 0);
+            ButtplugTab.SetApplicationTab("Buttplug Client", _clientPanel);
+
+            ButtplugTab.SelectedDevicesChanged += SelectionChangedHandler;
+        }
+
+        private void SelectionChangedHandler(object aObj, List<ButtplugDeviceInfo> aDevices)
+        {
+            foreach (var dev in aDevices)
+            {
+                var cdev = new ButtplugClientDevice(dev.Index, dev.Name, dev.Messages);
+                _clientPanel.Devices.AddOrUpdate(dev.Index, cdev, (idx, old) => cdev);
+            }
+
+            List<uint> ids = new List<uint>();
+            foreach (var dev in aDevices)
+            {
+                ids.Add(dev.Index);
+            }
+
+            foreach (var dev in _clientPanel.Devices.Values)
+            {
+                if (!ids.Contains(dev.Index))
+                {
+                    _clientPanel.Devices.TryRemove(dev.Index, out var old);
+                }
+            }
         }
     }
 }
