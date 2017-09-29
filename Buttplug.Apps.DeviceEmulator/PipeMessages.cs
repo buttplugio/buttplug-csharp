859d92101e999d3becaf1f6860ea0dfd61c59b08
blackspherefollower <darktech@black-sphere.co.uk>
2017-09-28 20:48:29 -0700

    Hooking up the simulation manger with the GUI
    This currently allows you to add devices to the simulator GUI, connect the client to the websocket server, connect the simulator GUI to the websocket server (via named pipes) and manipulate the simulator GUI with the client GUI
    
    This means we can test end-to-end without any NSFW content!

---
+++ Buttplug.Apps.DeviceEmulator/PipeMessages.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Buttplug.DeviceSimulator.PipeMessages
+{
+    public interface IDeviceSimulatorPipeMessage
+    {
+    }
+
+    public class ErrorMsg : IDeviceSimulatorPipeMessage
+    {
+        public string ErrorMessage;
+
+        public ErrorMsg(string aErrMessage)
+        {
+            ErrorMessage = aErrMessage;
+        }
+    }
+
+    public class StartScanning : IDeviceSimulatorPipeMessage
+    {
+        public StartScanning()
+        {
+        }
+    }
+
+    public class StopScanning : IDeviceSimulatorPipeMessage
+    {
+        public StopScanning()
+        {
+        }
+    }
+
+    public class FinishedScanning : IDeviceSimulatorPipeMessage
+    {
+        public FinishedScanning()
+        {
+        }
+    }
+
+    public class DeviceAdded : IDeviceSimulatorPipeMessage
+    {
+        public string Name;
+
+        public string Id;
+
+        public DeviceAdded()
+        {
+        }
+    }
+
+    public class DeviceRemoved : IDeviceSimulatorPipeMessage
+    {
+        public DeviceRemoved()
+        {
+        }
+    }
+
+    internal class StopDevice : IDeviceSimulatorPipeMessage
+    {
+        public string Id;
+
+        public StopDevice(string aId)
+        {
+            Id = aId;
+        }
+    }
+    class Vibrate : IDeviceSimulatorPipeMessage
+    {
+        public string Id;
+        public double Speed;
+
+        public Vibrate(string aId, double aSpeed)
+        {
+            Id = aId;
+            Speed = aSpeed;
+        }
+    }
+
+    public class PipeMessageParser
+    {
+        private readonly Dictionary<string, Type> _messageTypes;
+
+        public PipeMessageParser()
+        {
+            Type[] allTypes;
+            try
+            {
+                allTypes = Assembly.GetAssembly(typeof(IDeviceSimulatorPipeMessage)).GetTypes();
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                allTypes = e.Types;
+            }
+
+            var messageClasses = from t in allTypes
+                                 where t != null && t.IsClass && t.Namespace == "Buttplug.DeviceSimulator.PipeMessages" && typeof(IDeviceSimulatorPipeMessage).IsAssignableFrom(t)
+                                 select t;
+
+            var enumerable = messageClasses as Type[] ?? messageClasses.ToArray();
+            _messageTypes = new Dictionary<string, Type>();
+            enumerable.ToList().ForEach(aMessageType =>
+            {
+                _messageTypes.Add(aMessageType.Name, aMessageType);
+            });
+        }
+
+        public string Serialize(IDeviceSimulatorPipeMessage aMsg)
+        {
+            // Warning: Any log messages in this function must be localOnly. They will possibly recurse.
+            var o = new JObject(new JProperty(aMsg.GetType().Name, JObject.FromObject(aMsg)));
+            return o.ToString(Formatting.None);
+        }
+
+        public IDeviceSimulatorPipeMessage Deserialize(string aJsonMsg)
+        {
+            try
+            {
+                var o = JObject.Parse(aJsonMsg);
+                if (!o.Properties().Any())
+                {
+                    return new ErrorMsg("No message name available");
+                }
+
+                var msgName = o.Properties().First().Name;
+                if (!_messageTypes.Keys.Any() || !_messageTypes.Keys.Contains(msgName))
+                {
+                    return new ErrorMsg($"{msgName} is not a valid message class");
+                }
+
+                var s = new JsonSerializer { MissingMemberHandling = MissingMemberHandling.Error };
+
+                // This specifically could fail due to object conversion.
+                try
+                {
+                    var r = o[msgName].Value<JObject>();
+                    return (IDeviceSimulatorPipeMessage)r.ToObject(_messageTypes[msgName], s);
+                }
+                catch (InvalidCastException e)
+                {
+                    return new ErrorMsg($"Could not create message for JSON {aJsonMsg}: {e.Message}");
+                }
+                catch (JsonSerializationException e)
+                {
+                    return new ErrorMsg($"Could not create message for JSON {aJsonMsg}: {e.Message}");
+                }
+            }
+            catch (JsonReaderException e)
+            {
+                return new ErrorMsg($"Not valid JSON: {aJsonMsg} - {e.Message}");
+            }
+        }
+    }
+}
+
--- Buttplug.Server.Managers.SimulatorManager/SimulatorManager.cs
+++ Buttplug.Server.Managers.SimulatorManager/SimulatorManager.cs
@@ -1,57 +1,153 @@
-﻿
-using Buttplug.Core;
-using System.Net;
-using System.Net.Sockets;
+﻿using System;
+using System.IO.Pipes;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Buttplug.Core;
+using Buttplug.DeviceSimulator.PipeMessages;
 
 namespace Buttplug.Server.Managers.SimulatorManager
 {
     public class SimulatorManager : DeviceSubtypeManager
     {
-        private TcpListener _socket;
+        private NamedPipeServerStream _pipeServer;
 
         private Task _readThread;
 
         private CancellationTokenSource _tokenSource;
 
+        private bool _scanning;
+
+        private PipeMessageParser _parser;
+
+        private ButtplugLogManager _logManager;
+
         public SimulatorManager(IButtplugLogManager aLogManager)
             : base(aLogManager)
         {
             BpLogger.Info("Loading Simulator Manager");
-            _socket = new TcpListener(new IPAddress(new byte[] { 127, 0, 0, 1 }), 54321);
-            _socket.Start();
+            _scanning = false;
+
+            _parser = new PipeMessageParser();
+
+            _logManager = new ButtplugLogManager();
+
+            _pipeServer = new NamedPipeServerStream("ButtplugDeviceSimulator", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
 
             _tokenSource = new CancellationTokenSource();
             _readThread = new Task(() => { connAccepter(_tokenSource.Token); }, _tokenSource.Token, TaskCreationOptions.LongRunning);
             _readThread.Start();
         }
 
+        internal void Vibrate(SimulatedButtplugDevice aDev, double aSpeed)
+        {
+            if (_pipeServer.IsConnected)
+            {
+                var msg = _parser.Serialize(new Vibrate(aDev.Identifier, aSpeed));
+                _pipeServer.Write(Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            }
+        }
+
+        internal void StopDevice(SimulatedButtplugDevice aDev)
+        {
+            if (_pipeServer.IsConnected)
+            {
+                var msg = _parser.Serialize(new StopDevice(aDev.Identifier));
+                _pipeServer.Write(Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            }
+        }
+
         private void connAccepter(CancellationToken aCancellationToken)
         {
-            if (_socket.Pending())
+            while (!aCancellationToken.IsCancellationRequested)
             {
-                var client = _socket.AcceptTcpClient();
+                if (!_pipeServer.IsConnected)
+                {
+                    _scanning = false;
+                    _pipeServer.WaitForConnection();
+                    pipeReader(aCancellationToken);
+                }
+                else
+                {
+                    Thread.Sleep(500);
+                }
             }
-            else
+        }
+
+        private void pipeReader(CancellationToken aCancellationToken)
+        {
+            while (!aCancellationToken.IsCancellationRequested && _pipeServer.IsConnected)
             {
-                Thread.Sleep(500);
+                var buffer = new byte[4096];
+                string msg = string.Empty;
+                var len = -1;
+                while (len < 0 || (len == buffer.Length && buffer[4095] != '\0'))
+                {
+                    var waiter = _pipeServer.ReadAsync(buffer, 0, buffer.Length);
+                    while (!waiter.GetAwaiter().IsCompleted)
+                    {
+                        if (!_pipeServer.IsConnected)
+                        {
+                            return;
+                        }
+
+                        Thread.Sleep(100);
+                    }
+
+                    len = waiter.GetAwaiter().GetResult();
+
+                    if (len > 0)
+                    {
+                        msg += Encoding.ASCII.GetString(buffer, 0, len);
+                    }
+                }
+
+                switch (_parser.Deserialize(msg))
+                {
+                    case FinishedScanning fs:
+                        InvokeScanningFinished();
+                        _scanning = false;
+                        break;
+
+                    case DeviceAdded da:
+                        InvokeDeviceAdded(new DeviceAddedEventArgs(new SimulatedButtplugDevice(this, _logManager, da.Name, da.Id)));
+                        break;
+
+                    case DeviceRemoved dr:
+                        //InvokeDevice (new DeviceAddedEventArgs(new SimulatedButtplugDevice(_logManager, "Test", "1234")));
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
 
         public override void StartScanning()
         {
             BpLogger.Info("SimulatorManager start scanning");
+            if (_pipeServer.IsConnected)
+            {
+                _scanning = true;
+                var msg = _parser.Serialize(new StartScanning());
+                _pipeServer.Write(Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            }
         }
 
         public override void StopScanning()
         {
             BpLogger.Info("SimulatorManager stop scanning");
+            if (_pipeServer.IsConnected)
+            {
+                _scanning = false;
+                var msg = _parser.Serialize(new StopScanning());
+                _pipeServer.Write(Encoding.ASCII.GetBytes(msg), 0, msg.Length);
+            }
         }
 
         public override bool IsScanning()
         {
-            return false;
+            return _scanning;
         }
     }
 }
