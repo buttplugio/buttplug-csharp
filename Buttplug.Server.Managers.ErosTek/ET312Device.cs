6b9dc7e6f27ff16735f08ea9fc94a756db270e36
Orgasmscience <orgasmscience@gmail.com>
2017-11-01 13:34:24 +0100

    Initial Implementation of ErosTek E-Stim device detection.

---
+++ Buttplug.Server.Managers.ErosTek/ET312Device.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using Buttplug.Core;
+
+namespace Buttplug.Server.Managers.ETSerialManager
+{
+
+    public class ET312HandshakeException : Exception
+    {
+        public ET312HandshakeException()
+        {
+        }
+
+        public ET312HandshakeException(string message)
+            : base(message)
+        {
+        }
+
+        public ET312HandshakeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    public class ET312Device : ButtplugDevice
+    {
+        public ET312Device(SerialPort port, IButtplugLogManager aLogManager, string name, string id)
+            : base(aLogManager, name, id)
+        {
+
+
+        }
+
+        public override void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Buttplug.Server.Managers.ErosTek/ETSerialManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using Buttplug.Core;
+using JetBrains.Annotations;
+
+namespace Buttplug.Server.Managers.ETSerialManager
+{
+    public class ETSerialManager : DeviceSubtypeManager
+    {
+        private bool _isScanning = false;
+        private Thread _scanThread;
+        private string[] _searchComPorts;
+
+        public ETSerialManager(IButtplugLogManager aLogManager)
+            : base(aLogManager)
+        {
+            BpLogger.Info("Loading ErosTek Serial Port Manager");
+        }
+
+        public override void StartScanning()
+        {
+            BpLogger.Info("Starting Scanning Serial Ports for ErosTek Devices");
+            _scanThread = new Thread(() => ScanSerialPorts(_searchComPorts));
+            _scanThread.Start();
+        }
+
+        public override void StopScanning()
+        {
+            BpLogger.Info("Stopping Scanning Serial Ports for ErosTek Devices");
+            _isScanning = false;
+            if (_scanThread.IsAlive)
+            {
+                _scanThread.Join();
+            }
+        }
+
+        public override bool IsScanning()
+        {
+            return _isScanning;
+        }
+
+        private void ScanSerialPorts(string[] selectedComPorts)
+        {
+            string[] comPortsToScan;
+            _isScanning = true;
+
+            while (_isScanning)
+            {
+                // Enumerate Ports
+                if (selectedComPorts == null || selectedComPorts.Length == 0)
+                {
+                    comPortsToScan = SerialPort.GetPortNames();
+                }
+                else
+                {
+                    comPortsToScan = selectedComPorts;
+                }
+
+                // try to detect devices on all port
+                foreach (string port in comPortsToScan)
+                {
+                    BpLogger.Info("Scanning "+port);
+
+                    SerialPort serialPort = new SerialPort(port);
+
+                    serialPort.ReadTimeout = 200;
+                    serialPort.WriteTimeout = 200;
+                    serialPort.BaudRate = 19200;
+                    serialPort.Parity = Parity.None;
+                    serialPort.StopBits = StopBits.One;
+                    serialPort.DataBits = 8;
+                    serialPort.Handshake = Handshake.None;
+
+                    try
+                    {
+                        serialPort.Open();
+                    }
+                    catch
+                    {
+                        // This port is inaccessible.
+                        // Possibly because a device detected earlier is already using it.
+                        continue;
+                    }
+
+                    // We send 0x00 up to 11 times until we get 0x07 back.
+                    // Why 11? See et312-protocol.org
+                    var detected = false;
+
+                    for (int i = 0; i < 11; i++)
+                    {
+                        serialPort.Write(new byte[] { 0x00 }, 0, 1);
+
+                        try
+                        {
+                            if (serialPort.ReadByte() == 0x07)
+                            {
+                                detected = true;
+                                break;
+                            }
+                        }
+                        catch (TimeoutException)
+                        {
+                            // No response? Keep trying.
+                            continue;
+                        }
+                        catch
+                        {
+                            // Can't read from this port? Stop trying.
+                            break;
+                        }
+                    }
+
+                    if (detected)
+                    {
+                        // This seems to be an ET312! Let's try to create the device.
+                        try
+                        {
+                            BpLogger.Info("Found device at port " + port);
+
+                            var device = new ET312Device(serialPort, LogManager, "Erostek ET-312", port);
+
+                            // Device succesfully created!
+                            InvokeDeviceAdded(new DeviceAddedEventArgs(device));
+                            continue;
+                        }
+                        catch (ET312HandshakeException)
+                        {
+                            // Sync Failed. Not the device we were expecting or comms garbled.
+                        }
+                    }
+
+                    // No useful device detected on this port. Close the port.
+                    serialPort.Close();
+                    serialPort.Dispose();
+                }
+
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+    }
+}
