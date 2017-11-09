5e1f18ac4babf351e5bfe61345d7287bf767532e
Orgasmscience <orgasmscience@gmail.com>
2017-11-09 18:07:35 +0100

    Moving Exceptions into separate files and Minor cleanups.

---
+++ Buttplug.Server.Managers.ErosTek/ET312HandshakeException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Buttplug.Server.Managers.ETSerialManager
+{
+    public class ET312HandshakeException : Exception
+    {
+        public ET312HandshakeException()
+        {
+        }
+
+        public ET312HandshakeException(string message)
+            : base(message)
+        {
+        }
+
+        public ET312HandshakeException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
