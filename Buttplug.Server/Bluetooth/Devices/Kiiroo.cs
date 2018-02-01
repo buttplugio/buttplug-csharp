3a68509c428861bedec5f6da365af7913cbaa8f2
blackspherefollower <darktech@black-sphere.co.uk>
2018-02-01 04:58:08 -0800

    Fixing Gen1 Kiiroo driver
    Finally got the Onyx to respond reliably. Needs fixing up.

--- Buttplug.Server/Bluetooth/Devices/Kiiroo.cs
+++ Buttplug.Server/Bluetooth/Devices/Kiiroo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Buttplug.Core;
 using Buttplug.Core.Messages;
@@ -10,13 +11,33 @@ namespace Buttplug.Server.Bluetooth.Devices
 {
     internal class KiirooBluetoothInfo : IBluetoothDeviceInfo
     {
+        public enum Chrs : uint
+        {
+            Rx = 0,
+            Tx,
+            Cmd,
+            Cmd2,
+        }
         public string[] Names { get; } = { "ONYX", "PEARL" };
 
-        public string[] NamePrefixes { get;  } = { };
+        public string[] NamePrefixes { get; } = { };
 
         public Guid[] Services { get; } = { new Guid("49535343-fe7d-4ae5-8fa9-9fafd205e455") };
 
-        public Dictionary<uint, Guid> Characteristics { get; } = new Dictionary<uint, Guid>();
+        public Dictionary<uint, Guid> Characteristics { get; } = new Dictionary<uint, Guid>()
+        {
+            // rx
+            { (uint)Chrs.Rx, new Guid("49535343-1e4d-4bd9-ba61-23c647249616") },
+
+            // tx
+            { (uint)Chrs.Tx, new Guid("49535343-8841-43f4-a8d4-ecbe34729bb3") },
+
+            // cmd
+            { (uint)Chrs.Cmd, new Guid("49535343-aca3-481c-91ec-d85e28a60318") },
+
+            // cmd2
+            { (uint)Chrs.Cmd2, new Guid("49535343-6daa-4d02-abf6-19569aca69fe") },
+        };
 
         public IButtplugDevice CreateDevice(IButtplugLogManager aLogManager,
             IBluetoothDeviceInterface aInterface)
@@ -27,7 +48,13 @@ namespace Buttplug.Server.Bluetooth.Devices
 
     internal class Kiiroo : ButtplugBluetoothDevice
     {
-        private double _vibratorSpeed;
+        private readonly object _onyxLock = new object();
+        private double _deviceSpeed;
+        private double _targetPosition;
+        private double _currentPosition;
+        private DateTime _targetTime = DateTime.Now;
+        private DateTime _currentTime = DateTime.Now;
+        private Timer _onyxTimer;
 
         public Kiiroo([NotNull] IButtplugLogManager aLogManager,
                       [NotNull] IBluetoothDeviceInterface aInterface,
@@ -40,10 +67,148 @@ namespace Buttplug.Server.Bluetooth.Devices
             MsgFuncs.Add(typeof(KiirooCmd), new ButtplugDeviceWrapper(HandleKiirooRawCmd));
             MsgFuncs.Add(typeof(StopDeviceCmd), new ButtplugDeviceWrapper(HandleStopDeviceCmd));
 
+            // ReSharper disable once ConvertIfStatementToSwitchStatement
             if (aInterface.Name == "PEARL")
             {
-                MsgFuncs.Add(typeof(VibrateCmd), new ButtplugDeviceWrapper(HandleVibrateCmd, new MessageAttributes() { FeatureCount = 1 }));
+                MsgFuncs.Add(typeof(VibrateCmd), new ButtplugDeviceWrapper(HandleVibrateCmd,
+                    new MessageAttributes() { FeatureCount = 1 }));
                 MsgFuncs.Add(typeof(SingleMotorVibrateCmd), new ButtplugDeviceWrapper(HandleSingleMotorVibrateCmd));
+            }
+            else if (aInterface.Name == "ONYX")
+            {
+                MsgFuncs.Add(typeof(LinearCmd), new ButtplugDeviceWrapper(HandleLinearCmd,
+                    new MessageAttributes() { FeatureCount = 1 }));
+            }
+        }
+
+        private void OnBluetoothMessageReceived(object sender, BluetoothNotifyEventArgs aArgs)
+        {
+            BpLogger.Trace($"Kirroo sent data: {BitConverter.ToString(aArgs.bytes)}");
+        }
+
+        public override async Task<ButtplugMessage> Initialize()
+        {
+            // Start listening for incoming
+            Interface.BluetoothNotifyReceived += OnBluetoothMessageReceived;
+            await Interface.SubscribeToUpdates(ButtplugConsts.SystemMsgId, (int)KiirooBluetoothInfo.Chrs.Rx);
+
+            // Tell the Onyx to behave?
+            // Not really sure what we're doing here, but this is what we see in the wild
+
+            // Mode select?
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd,
+                new byte[] { 0x01, 0x00 }, true);
+
+            // Twice for luck?
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd,
+                new byte[] { 0x01, 0x00 }, true);
+
+            // Set to start position
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Tx,
+                new byte[] { 0x30, 0x2c }, true);
+
+            // Version request?
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Tx,
+                new byte[] { 0x76 }, true);
+
+            // Handshake? Works for Onyx...
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd2,
+                new byte[] { 0xff, 0x10, 0x00, 0x20, 0x00, 0x00, 0x00, 0x64, 0x00 }, true);
+
+            // Handshake? Twice for luck?
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd2,
+                new byte[] { 0xff, 0x10, 0x00, 0x20, 0x00, 0x00, 0x00, 0x64, 0x00 }, true);
+
+            // Mode request?
+            var res = await Interface.ReadValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd2);
+            BpLogger.Trace($"Kirroo read data: {BitConverter.ToString(res.Item2)}");
+
+            // Set to start position again?
+            await Interface.WriteValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Tx,
+                new byte[] { 0x30, 0x2c }, true);
+
+            // Mode request?
+            res = await Interface.ReadValue(ButtplugConsts.SystemMsgId,
+                (uint)KiirooBluetoothInfo.Chrs.Cmd2);
+            BpLogger.Trace($"Kirroo read data: {BitConverter.ToString(res.Item2)}");
+
+            if (Interface.Name != "ONYX")
+            {
+                return new Ok(ButtplugConsts.SystemMsgId);
+            }
+
+            // Onyx specific
+
+            Interface.DeviceRemoved += (sender, args) =>
+            {
+                _onyxTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+                _onyxTimer = null;
+            };
+
+            _currentTime = DateTime.Now;
+            _onyxTimer = new Timer(OnOnyxTimer, null, 500, 500);
+
+            return new Ok(ButtplugConsts.SystemMsgId);
+        }
+
+        public override void Disconnect()
+        {
+            if (Interface.Name == "ONYX")
+            {
+                _onyxTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+                _onyxTimer = null;
+            }
+
+            Interface.Disconnect();
+        }
+
+        private async void OnOnyxTimer(object state)
+        {
+            lock (_onyxLock)
+            {
+                // Given the time since the last iteration and the difference in distance, work out have far we should move
+                var distance = Math.Abs(_targetPosition - _currentPosition);
+                var oldTime = new DateTime(_currentTime.Ticks);
+                var nowTime = DateTime.Now;
+                var delta = nowTime.Subtract(oldTime);
+
+                if (Convert.ToUInt32(distance * 4) == 0 && delta.TotalMilliseconds < 500)
+                {
+                    // Skip. We do want to occationally ping the Onyx, but only every half a second
+                    return;
+                }
+
+                _currentTime = DateTime.Now;
+
+                if (_currentTime.CompareTo(_targetTime) >= 0 || distance < 0.000001)
+                {
+                    // We've overdue: jump to target
+                    _currentPosition = _targetPosition;
+                    _onyxTimer?.Change(500, 500);
+                }
+                else
+                {
+                    // The hard part: find the persentage time gone, then add that percentate of the movement delta
+                    var delta2 = _targetTime.Subtract(oldTime);
+
+                    var move = Convert.ToDouble(delta.TotalMilliseconds) / (Convert.ToDouble(delta2.TotalMilliseconds) + 1) * distance;
+                    _currentPosition += move * (_targetPosition > _currentPosition ? 1 : -1);
+                    _currentPosition = Math.Max(0, Math.Min(1, _currentPosition));
+                }
+            }
+
+            var res = await HandleKiirooRawCmd(new KiirooCmd(0, Convert.ToUInt32(_currentPosition * 4), ButtplugConsts.SystemMsgId));
+            if (res is Error err)
+            {
+                BpLogger.Error(err.ErrorMessage);
             }
         }
 
@@ -54,7 +219,7 @@ namespace Buttplug.Server.Bluetooth.Devices
             // never gotten that to work. So for now, we just return ok.
             BpLogger.Debug("Stopping Device " + Name);
 
-            if (Interface.Name == "PEARL" && _vibratorSpeed > 0)
+            if (Interface.Name == "PEARL" && _deviceSpeed > 0)
             {
                 return await HandleKiirooRawCmd(new KiirooCmd(aMsg.DeviceIndex, 0, aMsg.Id));
             }
@@ -69,23 +234,23 @@ namespace Buttplug.Server.Bluetooth.Devices
                 return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
             }
 
-            return await Interface.WriteValue(cmdMsg.Id,
+            return await Interface.WriteValue(cmdMsg.Id, (uint)KiirooBluetoothInfo.Chrs.Tx,
                 Encoding.ASCII.GetBytes($"{cmdMsg.Position},\n"));
         }
 
         private async Task<ButtplugMessage> HandleSingleMotorVibrateCmd([NotNull] ButtplugDeviceMessage aMsg)
         {
-            if (!(aMsg is SingleMotorVibrateCmd cmdMsg))
+            if (!(aMsg is SingleMotorVibrateCmd cmdMsg) || Interface.Name != "PEARL")
             {
                 return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
             }
 
             return await HandleVibrateCmd(VibrateCmd.Create(cmdMsg.DeviceIndex, cmdMsg.Id, cmdMsg.Speed, 1));
-        }
+    }
 
         private async Task<ButtplugMessage> HandleVibrateCmd([NotNull] ButtplugDeviceMessage aMsg)
         {
-            if (!(aMsg is VibrateCmd cmdMsg))
+            if (!(aMsg is VibrateCmd cmdMsg) || Interface.Name != "PEARL")
             {
                 return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
             }
@@ -108,10 +273,54 @@ namespace Buttplug.Server.Bluetooth.Devices
                         cmdMsg.Id);
                 }
 
-                _vibratorSpeed = v.Speed;
+                if (Math.Abs(_deviceSpeed - v.Speed) < 0.001)
+                {
+                    return new Ok(cmdMsg.Id);
+                }
+
+                _deviceSpeed = v.Speed;
             }
 
-            return await HandleKiirooRawCmd(new KiirooCmd(aMsg.DeviceIndex, Convert.ToUInt16(_vibratorSpeed * 4), aMsg.Id));
+            return await HandleKiirooRawCmd(new KiirooCmd(aMsg.DeviceIndex, Convert.ToUInt16(_deviceSpeed * 4), aMsg.Id));
+        }
+
+        private Task<ButtplugMessage> HandleLinearCmd([NotNull] ButtplugDeviceMessage aMsg)
+        {
+            if (!(aMsg is LinearCmd cmdMsg) || Interface.Name != "ONYX")
+            {
+                return Task.FromResult<ButtplugMessage>(BpLogger.LogErrorMsg(
+                    aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler"));
+            }
+
+            if (cmdMsg.Vectors.Count != 1)
+            {
+                return Task.FromResult<ButtplugMessage>(new Error(
+                    "LinearCmd requires 1 vector for this device.",
+                    Error.ErrorClass.ERROR_DEVICE,
+                    cmdMsg.Id));
+            }
+
+            foreach (var v in cmdMsg.Vectors)
+            {
+                if (v.Index != 0)
+                {
+                    return Task.FromResult<ButtplugMessage>(new Error(
+                        $"Index {v.Index} is out of bounds for LinearCmd for this device.",
+                        Error.ErrorClass.ERROR_DEVICE,
+                        cmdMsg.Id));
+                }
+
+                // Invert the position
+                lock (_onyxLock)
+                {
+                    _targetPosition = 1 - v.Position;
+                    _currentTime = DateTime.Now;
+                    _targetTime = DateTime.Now.AddMilliseconds(v.Duration);
+                    _onyxTimer?.Change(0, 50);
+                }
+            }
+
+            return Task.FromResult<ButtplugMessage>(new Ok(aMsg.Id));
         }
     }
 }
