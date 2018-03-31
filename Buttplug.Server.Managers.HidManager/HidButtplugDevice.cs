349accdd4c75ea0dd006831c4dccab458f3a6a3b
blackspherefollower <darktech@black-sphere.co.uk>
2018-03-30 22:19:54 -0700

    feat: Cyclone X10 support
    This may or may not have been the precursor to the Vorze A10 Cyclone.
    Assuming that it might be, it is backwards compatible with the VorzeA10CycloneCmd

---
+++ Buttplug.Server.Managers.HidManager/HidButtplugDevice.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Buttplug.Core;
+using HidLibrary;
+
+namespace Buttplug.Server.Managers.HidManager
+{
+    internal abstract class HidButtplugDevice : ButtplugDevice, IDisposable
+    {
+        private readonly IHidDevice _hid;
+
+        private readonly CancellationTokenSource _tokenSource;
+
+        public readonly IHidDeviceInfo DeviceInfo;
+
+        public readonly IButtplugLogManager LogManager;
+
+        private bool _disposed;
+
+        private bool _reading;
+
+        private Task _readerThread;
+
+        protected HidButtplugDevice(IButtplugLogManager aLogManager, IHidDevice aHid, IHidDeviceInfo aDeviceInfo)
+            : base(aLogManager, aDeviceInfo.Name, aHid.DevicePath)
+        {
+            LogManager = aLogManager;
+            _hid = aHid;
+            DeviceInfo = aDeviceInfo;
+
+            _tokenSource = new CancellationTokenSource();
+            _hid.Inserted += DeviceAttachedHandler;
+            _hid.Removed += DeviceRemovedHandler;
+        }
+
+        public override void Disconnect()
+        {
+            _hid.CloseDevice();
+            InvokeDeviceRemoved();
+        }
+
+        public void BeginRead()
+        {
+            if (_readerThread != null && _readerThread.Status == TaskStatus.Running)
+            {
+                return;
+            }
+
+            _readerThread = new Task(ReportReader, _tokenSource.Token, TaskCreationOptions.LongRunning);
+            _readerThread.Start();
+        }
+
+        public void EndRead(bool close)
+        {
+            _reading = false;
+            _readerThread?.Wait();
+            _readerThread = null;
+            _hid.MonitorDeviceEvents = false;
+            if (close)
+            {
+                _hid.CloseDevice();
+            }
+        }
+
+        public List<byte> ReadData()
+        {
+            if (_disposed || !_hid.IsOpen)
+            {
+                return null;
+            }
+
+            var data = _hid.Read(100);
+            return data.Status != HidDeviceData.ReadStatus.Success ? null : data.Data.ToList();
+        }
+
+        public bool WriteData(byte[] aData)
+        {
+            if (_disposed || !_hid.IsOpen)
+            {
+                return false;
+            }
+
+            return _hid.Write(aData, 100);
+        }
+
+        private static void DeviceAttachedHandler()
+        {
+        }
+
+        private void DeviceRemovedHandler()
+        {
+            _reading = false;
+            InvokeDeviceRemoved();
+        }
+
+        private void ReportReader()
+        {
+            if (!_hid.IsOpen)
+            {
+                _hid.OpenDevice();
+            }
+
+            _reading = true;
+            _hid.MonitorDeviceEvents = true;
+            _hid.ReadReport(OnReport, 100);
+        }
+
+        private void OnReport(HidReport report)
+        {
+            if (!report.Exists || report.ReadStatus != HidDeviceData.ReadStatus.Success)
+            {
+                return;
+            }
+
+            if (HandleData(report.Data) && _reading)
+            {
+                _hid.ReadReport(OnReport);
+            }
+        }
+
+        protected abstract bool HandleData(byte[] data);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool aDisposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (aDisposing)
+            {
+                _hid.CloseDevice();
+                InvokeDeviceRemoved();
+            }
+
+            _disposed = true;
+        }
+    }
+}
---
+++ Buttplug.Server.Managers.HidManager/HidManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Buttplug.Core;
+using Buttplug.Server.Managers.HidManager.Devices;
+using HidLibrary;
+
+namespace Buttplug.Server.Managers.HidManager
+{
+    public class HidManager : DeviceSubtypeManager
+    {
+        private readonly List<HidDeviceFactory> _deviceFactories;
+
+        private readonly IButtplugLogManager _logger;
+
+        private bool _scanning;
+
+        public HidManager(IButtplugLogManager aLogger)
+            : base(aLogger)
+        {
+            _logger = aLogger;
+            _deviceFactories = new List<HidDeviceFactory>() { new HidDeviceFactory(aLogger, new CycloneX10HidDeviceInfo()) };
+        }
+
+        public override void StartScanning()
+        {
+            _scanning = true;
+            var hids = new HidEnumerator();
+            foreach (var hid in hids.Enumerate())
+            {
+                try
+                {
+                    hid.ReadProduct(out var product);
+                    hid.ReadManufacturer(out var vendor);
+                    var prod = Encoding.Unicode.GetString(product);
+                    var vend = Encoding.Unicode.GetString(vendor);
+                    prod = prod.Substring(0, prod.IndexOf('\0'));
+                    vend = vend.Substring(0, vend.IndexOf('\0'));
+
+                    _logger?.GetLogger(GetType()).Trace("Found HID device (" +
+                        hid.Attributes.VendorHexId + ":" + hid.Attributes.ProductHexId +
+                        "): " + vend + " - " + prod );
+
+                    var factories = _deviceFactories.Where(x =>
+                        x.MayBeDevice(hid.Attributes.VendorId, hid.Attributes.ProductId));
+                    var buttplugHidDeviceFactories = factories as HidDeviceFactory[] ?? factories.ToArray();
+                    if (buttplugHidDeviceFactories.Length != 1)
+                    {
+                        if (buttplugHidDeviceFactories.Any())
+                        {
+                            BpLogger.Warn($"Found multiple HID factories for " + hid.Attributes.VendorHexId + ":" + hid.Attributes.ProductHexId);
+                            buttplugHidDeviceFactories.ToList().ForEach(x => BpLogger.Warn(x.GetType().Name));
+                        }
+                        else
+                        {
+                            // BpLogger.Trace("No BLE factories found for device.");
+                        }
+
+                        continue;
+                    }
+
+                    var factory = buttplugHidDeviceFactories.First();
+                    BpLogger.Debug($"Found HID factory: {factory.GetType().Name}");
+
+                    var d = factory.CreateDevice(hid);
+                    InvokeDeviceAdded(new DeviceAddedEventArgs(d));
+                }
+                catch (Exception e)
+                {
+                    _logger?.GetLogger(GetType()).LogException(e);
+                }
+            }
+
+            _scanning = false;
+            InvokeScanningFinished();
+            }
+
+        public override void StopScanning()
+        {
+        }
+
+        public override bool IsScanning()
+        {
+            return _scanning;
+        }
+    }
+}
