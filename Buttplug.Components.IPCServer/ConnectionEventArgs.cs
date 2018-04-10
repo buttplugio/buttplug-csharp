9b1741602d9c111bb6966d836ef3158bcc6be379
blackspherefollower <darktech@black-sphere.co.uk>
2018-04-10 01:16:55 -0700

    feat: Adding IPC server support using named pipes

---
+++ Buttplug.Components.IPCServer/ConnectionEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace Buttplug.Components.IPCServer
+{
+    public class IPCConnectionEventArgs
+    {
+        [NotNull]
+        public string ClientName;
+
+        public IPCConnectionEventArgs(string aClientName = "Unknown Client")
+        {
+            ClientName = aClientName;
+        }
+    }
+}
