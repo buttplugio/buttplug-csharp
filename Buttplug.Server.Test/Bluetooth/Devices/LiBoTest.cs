2199e45af436c80b9bd59c6a7e69ac999f45c4a3
Kyle Machulis <kyle@machul.is>
2018-05-20 19:27:57 -0700

    chore: Bring LiBo code up to date
    - Actually add LiBo code to VS projects
    - Change internal structure to match new BluetoothInfo format
    - Remove unused code
    - Add tests
    
    Affects #410, #411

---
+++ Buttplug.Server.Test/Bluetooth/Devices/LiBoTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buttplug.Core.Messages;
+using Buttplug.Server.Bluetooth.Devices;
+using Buttplug.Server.Test.Util;
+using JetBrains.Annotations;
+using NUnit.Framework;
+
+namespace Buttplug.Server.Test.Bluetooth.Devices
+{
+    internal class LiBoTest
+    {
+        [NotNull]
+        private BluetoothDeviceTestUtils<LiBoBluetoothInfo> testUtil;
+
+        [SetUp]
+        public void Init()
+        {
+            testUtil = new BluetoothDeviceTestUtils<LiBoBluetoothInfo>();
+            testUtil.SetupTest("PiPiJing");
+        }
+
+        [Test]
+        public void TestAllowedMessages()
+        {
+            testUtil.TestDeviceAllowedMessages(new Dictionary<System.Type, uint>()
+            {
+                { typeof(StopDeviceCmd), 0 },
+                { typeof(SingleMotorVibrateCmd), 0 },
+                { typeof(VibrateCmd), 1 },
+            });
+        }
+
+        // StopDeviceCmd noop test handled in GeneralDeviceTests
+
+        [Test]
+        public void TestStopDeviceCmd()
+        {
+            var expected =
+                new List<(byte[], uint)>()
+                {
+                    (new byte[] { 2 }, (uint)LiBoBluetoothInfo.Chrs.WriteVibrate),
+                };
+
+            testUtil.TestDeviceMessage(new SingleMotorVibrateCmd(4, 0.5), expected, false);
+
+            expected =
+                new List<(byte[], uint)>()
+                {
+                    (new byte[] { 0 }, (uint)LiBoBluetoothInfo.Chrs.WriteVibrate),
+                };
+
+            testUtil.TestDeviceMessage(new StopDeviceCmd(4), expected, false);
+        }
+
+        [Test]
+        public void TestSingleMotorVibrateCmd()
+        {
+            var expected =
+                new List<(byte[], uint)>()
+                {
+                    (new byte[] { 2 }, (uint)LiBoBluetoothInfo.Chrs.WriteVibrate),
+                };
+
+            testUtil.TestDeviceMessage(new SingleMotorVibrateCmd(4, 0.5), expected, false);
+        }
+
+        [Test]
+        public void TestVibrateCmd()
+        {
+            var expected =
+                new List<(byte[], uint)>()
+                {
+                    (new byte[] { 2 }, (uint)LiBoBluetoothInfo.Chrs.WriteVibrate),
+                };
+
+            testUtil.TestDeviceMessage(VibrateCmd.Create(4, 1, 0.5, 1), expected, false);
+        }
+
+        [Test]
+        public void TestInvalidVibrateCmd()
+        {
+            testUtil.TestInvalidVibrateCmd(1);
+        }
+    }
+}
--- Buttplug.Server/Bluetooth/Devices/LiBo.cs
+++ Buttplug.Server/Bluetooth/Devices/LiBo.cs
@@ -11,34 +11,37 @@ namespace Buttplug.Server.Bluetooth.Devices
     {
         public enum Chrs : uint
         {
-            Tx1 = 0,
-            Tx2,
-            Rx,
+            WriteShock = 0,
+            WriteVibrate,
+            ReadBattery,
         }
 
         public Guid[] Services { get; } =
         {
             new Guid("00006000-0000-1000-8000-00805f9b34fb"), // Write Service
-            new Guid("00006050-0000-1000-8000-00805f9b34fb"), // Read service (battery)
 
-            // I'm pretty sure that the 2nd one here is ignored due to a call to Services.First() in the bluetooth manager
+            // TODO Commenting out battery service until we can handle multiple services.
+
+            // new Guid("00006050-0000-1000-8000-00805f9b34fb"), // Read service (battery)
         };
+
+        public string[] NamePrefixes { get; } = { };
 
         public string[] Names { get; } =
         {
             "PiPiJing"
         };
 
-        public Guid[] Characteristics { get; } =
+        public Dictionary<uint, Guid> Characteristics { get; } = new Dictionary<uint, Guid>()
         {
             // tx1 characteristic
-            new Guid("00006001-0000-1000-8000-00805f9b34fb"), // Shock
+            { (uint)Chrs.WriteShock, new Guid("00006001-0000-1000-8000-00805f9b34fb") }, // Shock
 
             // tx2 characteristic
-            new Guid("00006002-0000-1000-8000-00805f9b34fb"), // VibeMode
+            { (uint)Chrs.WriteVibrate, new Guid("00006002-0000-1000-8000-00805f9b34fb") }, // VibeMode
 
             // rx characteristic
-            new Guid("00006051-0000-1000-8000-00805f9b34fb"), // Read for battery level
+            { (uint)Chrs.ReadBattery,  new Guid("00006051-0000-1000-8000-00805f9b34fb") }, // Read for battery level
         };
 
         public IButtplugDevice CreateDevice(IButtplugLogManager aLogManager,
@@ -51,21 +54,20 @@ namespace Buttplug.Server.Bluetooth.Devices
     internal class LiBo : ButtplugBluetoothDevice
     {
         private readonly uint _vibratorCount = 1;
-        private readonly uint _shockerCount = 1;
         private readonly double[] _vibratorSpeed = { 0 };
-        private readonly double[] _shockerIntensity = { 0 };
 
         public LiBo(IButtplugLogManager aLogManager,
                       IBluetoothDeviceInterface aInterface,
                       IBluetoothDeviceInfo aInfo)
             : base(aLogManager,
-                   $"LiBo Device ({aInterface.Name})",
+                   $"LiBo ({aInterface.Name})",
                    aInterface,
                    aInfo)
         {
             MsgFuncs.Add(typeof(SingleMotorVibrateCmd), new ButtplugDeviceWrapper(HandleSingleMotorVibrateCmd));
             MsgFuncs.Add(typeof(VibrateCmd), new ButtplugDeviceWrapper(HandleVibrateCmd, new MessageAttributes() { FeatureCount = _vibratorCount }));
-            // Here goes a handler for Estim shocking
+
+            // TODO Add a handler for Estim shocking, add a battery handler.
             MsgFuncs.Add(typeof(StopDeviceCmd), new ButtplugDeviceWrapper(HandleStopDeviceCmd));
         }
 
@@ -81,13 +83,7 @@ namespace Buttplug.Server.Bluetooth.Devices
                 return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
             }
 
-            var subCmds = new List<VibrateCmd.VibrateSubcommand>();
-            for (var i = 0u; i < _vibratorCount; i++)
-            {
-                subCmds.Add(new VibrateCmd.VibrateSubcommand(i, cmdMsg.Speed));
-            }
-
-            return await HandleVibrateCmd(new VibrateCmd(cmdMsg.DeviceIndex, subCmds, cmdMsg.Id));
+            return await HandleVibrateCmd(VibrateCmd.Create(cmdMsg.DeviceIndex, cmdMsg.Id, cmdMsg.Speed, _vibratorCount));
         }
 
         private async Task<ButtplugMessage> HandleVibrateCmd(ButtplugDeviceMessage aMsg)
@@ -97,7 +93,7 @@ namespace Buttplug.Server.Bluetooth.Devices
                 return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
             }
 
-            if (cmdMsg.Speeds.Count < 1 || cmdMsg.Speeds.Count > _vibratorCount)
+            if (cmdMsg.Speeds.Count != _vibratorCount)
             {
                 return new Error(
                     $"VibrateCmd requires between 1 and {_vibratorCount} vectors for this device.",
@@ -130,76 +126,14 @@ namespace Buttplug.Server.Bluetooth.Devices
                 return new Ok(cmdMsg.Id);
             }
 
-            int mode = (int)Math.Ceiling(_vibratorSpeed[0] * 3); // Map a 0 - 100% value to a 0 - 3 value since 0 * x == 0 this will turn off the vibe if speed is 0.00
+            // Map a 0 - 100% value to a 0 - 3 value since 0 * x == 0 this will turn off the vibe if
+            // speed is 0.00
+            int mode = (int)Math.Ceiling(_vibratorSpeed[0] * 3);
 
             var data = new byte[] { Convert.ToByte(mode) };
 
             return await Interface.WriteValue(aMsg.Id,
-                Info.Characteristics[(uint)LiBoBluetoothInfo.Chrs.Tx2],
-                data);
-        }
-
-        private int getBatteryLevel()
-        {
-            throw new NotImplementedException("While most of this method is done it still needs to be adjusted/completed - see the comments");
-            int batteryLevel = Convert.ToInt32(0x64 /* 100 in hex */);  // Read from Info.Services[1] + Info.Characteristics[uint)LiBoBluetoothInfo.Chrs.Rx] here
-            return batteryLevel;                                        // As far as I can tell we currently have no way of reading values
-        }
-
-        private async Task<ButtplugMessage> HandleShockCmd(ButtplugDeviceMessage aMsg)
-        {
-            throw new NotImplementedException("While most of this method is done it still needs to be adjusted/completed - see the comments");
-            if (!(aMsg is VibrateCmd cmdMsg)) // ShockCmd once it gets implemented
-            {
-                return BpLogger.LogErrorMsg(aMsg.Id, Error.ErrorClass.ERROR_DEVICE, "Wrong Handler");
-            }
-
-            if (cmdMsg.Speeds.Count < 1 || cmdMsg.Speeds.Count > _shockerCount)
-            {
-                return new Error(
-                    $"ShockCmd requires between 1 and {_shockerCount} vectors for this device.", // Fix cmd name once settled
-                    Error.ErrorClass.ERROR_DEVICE,
-                    cmdMsg.Id);
-            }
-
-            var changed = false;
-            foreach (var v in cmdMsg.Speeds)
-            {
-                if (v.Index >= _shockerCount)
-                {
-                    return new Error(
-                        $"Index {v.Index} is out of bounds for VibrateCmd for this device.", // Fix cmd name once settled
-                        Error.ErrorClass.ERROR_DEVICE,
-                        cmdMsg.Id);
-                }
-
-                if (!(Math.Abs(v.Speed - _shockerIntensity[v.Index]) > 0.001))
-                {
-                    continue;
-                }
-
-                changed = true;
-                _shockerIntensity[v.Index] = v.Speed;
-            }
-
-            if (!changed)
-            {
-                return new Ok(cmdMsg.Id);
-            }
-
-            int speed = 0;
-
-            // If intensity is set to 0 skip this with 0x00 and turn the shocker off
-            if (_shockerIntensity[0] > 0)
-            {
-                speed = (int)Math.Floor(_shockerIntensity[0] * 7) << 4; // Map a 0 - 100% value to a 0 - 6 value shift 4 to have 0xY0 where Y is speed
-                speed++; // add 1 to speed to get 0xY1 where Y is speed and 1 is mode 1 (constant shock)
-            }
-
-            var data = new byte[] { Convert.ToByte(speed) };
-
-            return await Interface.WriteValue(aMsg.Id,
-                Info.Characteristics[(uint)LiBoBluetoothInfo.Chrs.Tx1],
+                (uint)LiBoBluetoothInfo.Chrs.WriteVibrate,
                 data);
         }
     }
