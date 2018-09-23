84439fa43f762b06694424d1f68b4ede69ce5ac4
Kyle Machulis <kyle@machul.is>
2018-09-23 16:06:04 -0700

    build: Standardize msbuild project files, add Net47 targets
    Fixes #499

---
+++ Buttplug.Core/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+// Let test project see internals
+//[assembly: InternalsVisibleTo("Buttplug.Client.Test")]
+//[assembly: InternalsVisibleTo("Buttplug.Server.Test")]
