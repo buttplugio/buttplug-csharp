5e8f85b6487258b68620fd65ba98252b31d87521
Kyle Machulis <kyle@machul.is>
2018-09-29 23:39:51 -0700

    chore: Rename ET312 exception classes
    Make them match the rest of the device files

--- Buttplug.Server.Managers.SerialPortManager/ErostekET312HandshakeException.cs
+++ Buttplug.Server.Managers.SerialPortManager/ErostekET312HandshakeException.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ET312CommunicationException.cs" company="Nonpolynomial Labs LLC">
+﻿// <copyright file="ET312HandshakeException.cs" company="Nonpolynomial Labs LLC">
 // Buttplug C# Source Code File - Visit https://buttplug.io for more info about the project.
 // Copyright (c) Nonpolynomial Labs LLC. All rights reserved.
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root for full license information.
@@ -9,9 +9,9 @@ using System;
 namespace Buttplug.Server.Managers.SerialPortManager
 {
     // ReSharper disable once InconsistentNaming
-    public class ET312CommunicationException : Exception
+    public class ErostekET312HandshakeException : Exception
     {
-        public ET312CommunicationException(string message)
+        public ErostekET312HandshakeException(string message)
             : base(message)
         {
         }
