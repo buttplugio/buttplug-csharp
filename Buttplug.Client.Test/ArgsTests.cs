42c178de3c053e906ed48153ef6edd4d0f957408
Kyle Machulis <kyle@machul.is>
2018-10-19 23:27:39 -0700

    chore: Remove unused ButtplugClient.ErrorEventArgs class

--- Buttplug.Client.Test/ArgsTests.cs
+++ Buttplug.Client.Test/ArgsTests.cs
@@ -13,23 +13,6 @@ namespace Buttplug.Client.Test
     public class ArgsTests
     {
         [Test]
-        public void ErrorEventArgsTest1()
-        {
-            var arg = new ErrorEventArgs(
-                new Core.Messages.Error("foo", Core.Messages.Error.ErrorClass.ERROR_UNKNOWN, 0));
-            Assert.AreEqual("foo", arg.Message.ErrorMessage);
-            Assert.Null(arg.Exception);
-        }
-
-        [Test]
-        public void ErrorEventArgsTest2()
-        {
-            var arg = new ErrorEventArgs(new Exception("bar"));
-            Assert.AreEqual("bar", arg.Message.ErrorMessage);
-            Assert.AreEqual("bar", arg.Exception.Message);
-        }
-
-        [Test]
         public void LogEventArgsTest()
         {
             var arg = new LogEventArgs(new Core.Messages.Log(Core.Logging.ButtplugLogLevel.Debug, "test"));
