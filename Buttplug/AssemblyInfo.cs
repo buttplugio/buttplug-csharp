55fe2ee590eadfed784d0011392377e097013033
Kyle Machulis <kyle@machul.is>
2018-11-01 21:13:08 -0700

    test: Consolidate core/client/server test projects into one project
    Way easier to manage than 3 separate projects. Also converting to .Net
    Standard so we can test elsewhere.
    
    Affects #530
    Fixes #497

---
+++ Buttplug/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Nonpolynomial Labs LLC">
+// Buttplug C# Source Code File - Visit https://buttplug.io for more info about the project.
+// Copyright (c) Nonpolynomial Labs LLC. All rights reserved.
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Runtime.CompilerServices;
+
+// Let test project see internals
+[assembly: InternalsVisibleTo("Buttplug.Test")]
