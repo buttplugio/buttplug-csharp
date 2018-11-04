0253107ccee643597ee9244d2c260c3b7411d4af
Kyle Machulis <kyle@machul.is>
2018-11-03 18:39:48 -0700

    doc: Add more commands to full program example
    Mostly fleshing out the comments to the subfunctions

--- Buttplug.Examples.07.FullProgram/Program.cs
+++ Buttplug.Examples.07.FullProgram/Program.cs
@@ -53,8 +53,8 @@ namespace Buttplug.Examples._07.FullProgram
             // At this point, if you want to see everything that's happening, uncomment this block to
             // turn on logging. Warning, it might be pretty spammy.
 
-            void HandleLogMessage(object aObj, LogEventArgs aArgs) { Console.WriteLine($"LOG: {aArgs.Message.LogMessage}"); }
-            client.Log += HandleLogMessage; await client.RequestLogAsync(ButtplugLogLevel.Debug);
+            // void HandleLogMessage(object aObj, LogEventArgs aArgs) { Console.WriteLine($"LOG: {aArgs.Message.LogMessage}"); }
+            // client.Log += HandleLogMessage; await client.RequestLogAsync(ButtplugLogLevel.Debug);
 
             // Now we scan for devices. Since we didn't add any Subtype Managers yet, this will go
             // out and find them for us. They'll be reported in the logs as they are found.
@@ -67,9 +67,22 @@ namespace Buttplug.Examples._07.FullProgram
 
             client.DeviceAdded += HandleDeviceAdded;
 
+            void HandleDeviceRemoved(object aObj, DeviceRemovedEventArgs aArgs)
+            {
+                Console.WriteLine($"Device connected: {aArgs.Device.Name}");
+            }
+
+            client.DeviceRemoved += HandleDeviceRemoved;
+            // The structure here is gonna get a little weird now, because I'm using method scoped
+            // functions. We'll be defining our scanning function first, then running it just to find
+            // any devices up front. Then we'll define our command sender. Finally, with all of that done,
+            // we'll end up in our main menu
+
+            // Here's the scanning part. Pretty simple, just scan until the user hits a button. Any
+            // time a new device is found, print it so the user knows we found it.
             async Task ScanForDevices()
             {
-                Console.WriteLine("Scanning for devices until key is pressed.");
+                Console.WriteLine("Scanning for devices until key is pressed. Found devices will be printed to console.");
                 await client.StartScanningAsync();
                 await WaitForKey();
 
@@ -77,15 +90,28 @@ namespace Buttplug.Examples._07.FullProgram
                 await client.StopScanningAsync();
             }
 
-            // Scan for devices before we get to our menu.
+            // Scan for devices before we get to the main menu.
             await ScanForDevices();
 
+            // Now we define the device control menus. After we've scanned for devices, the user can
+            // use this menu to select a device, then select an action for that device to take.
             async Task ControlDevice()
             {
-                List<uint> options = new List<uint>();
                 // Controlling a device has 2 steps: selecting the device to control, and choosing
                 // which command to send. We'll just list the devices the client has available, then
-                // search the device message capabilities once that's done to figure out what we can send.
+                // search the device message capabilities once that's done to figure out what we can
+                // send. Note that this is using the Device Index, which is assigned by the device
+                // manager and may not be sequential (which is why we can't just use an array index).
+
+                // Of course, if we don't have any devices yet, that's not gonna work.
+                if (!client.Devices.Any())
+                {
+                    Console.WriteLine("No devices available. Please scan for a device.");
+                    return;
+                }
+
+                var options = new List<uint>();
+
                 foreach (var dev in client.Devices)
                 {
                     Console.WriteLine($"{dev.Index}. {dev.Name}");
@@ -99,12 +125,24 @@ namespace Buttplug.Examples._07.FullProgram
                 }
 
                 var device = client.Devices.First(dev => dev.Index == deviceChoice);
+
+                // Now that we've gotten a device, we need to choose an action for that device to
+                // take. For sake of simplicity, right now we'll just use the 3 generic commands available:
+                //
+                // - Vibrate
+                // - Rotate
+                // - Linear (stroke/oscillate)
+                //
+                // Each device supported by the Buttplug C# library supports at least one of these 3
+                // commands, so we know that the user will always have some option.
                 var commandTypes = device.AllowedMessages.Keys.Intersect(new[] { typeof(VibrateCmd), typeof(RotateCmd), typeof(LinearCmd) }).ToArray();
 
                 Console.WriteLine("Choose an action:");
                 uint i = 1;
                 foreach (var command in commandTypes)
                 {
+                    // We know all device commands end in "Cmd", so we can cut off the last 3
+                    // characters and just have the action. Handy, if hacky.
                     Console.WriteLine($"{i}. {command.Name.Substring(0, command.Name.Length - 3)}");
                     ++i;
                 }
@@ -115,6 +153,15 @@ namespace Buttplug.Examples._07.FullProgram
                     return;
                 }
 
+                // We've got a device, and a command to take on that device. Let's do this thing. For
+                // each command we'll either run at a speed, then stop, or move to a position, then
+                // back again. To ensure that we don't have to deal with concurrent commands (again,
+                // for sake of example simplicity, real world situations are gonna be far more
+                // dynamic than this), we'll just block while this action is happening.
+                //
+                // We'll wrap each of our commands in a ButtplugDeviceException try block, as a
+                // device might be disconnected between the time we enter the command menu and send
+                // the command, and we don't want to crash when that happens.
                 var cmdType = commandTypes[cmdChoice - 1];
 
                 // Pattern matching for switch blocks doesn't seem to work here. :(
@@ -165,6 +212,9 @@ namespace Buttplug.Examples._07.FullProgram
                 }
             }
 
+            // And finally, we arrive at the main menu. We give the user the choice to scan for more
+            // devices (in case they forgot to turn them on earlier or whatever), run a command on a
+            // device, or just quit.
             while (true)
             {
                 Console.WriteLine("1. Scan For More Devices\n2. Control Devices\n3. Quit\nChoose an option: ");
@@ -190,6 +240,12 @@ namespace Buttplug.Examples._07.FullProgram
                         continue;
                 }
             }
+
+            // That's it! A full buttplug program. It doesn't do much, but with the right toys, the
+            // right commands, and a user that doesn't possibly mind getting lube on their keyboard,
+            // this program could possibly get someone off.
+            //
+            // Mission Accomplished.
         }
 
         // Since not everyone is probably going to want to run under C# 7.1+, we'll use a non-async
