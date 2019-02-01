5f188b165d6d472b1cda8ded731342d8ce83a901
Kyle Machulis <kyle@machul.is>
2019-01-31 23:59:32 -0800

    feat: Add Uwp HID Manager
    Simple UWP Hid Manager for Windows 10

---
+++ Buttplug.Server.Managers.UwpHidManager/UwpHidDeviceInterface.cs
@@ -0,0 +1,77 @@
+﻿using Buttplug.Core.Logging;
+using Buttplug.Core.Messages;
+using Buttplug.Devices;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Storage.Streams;
+using Buttplug.Core;
+using Endpoints = Buttplug.Devices.Endpoints;
+
+namespace Buttplug.Server.Managers.UwpHidManager
+{
+    internal class UwpHidDeviceInterface : ButtplugDeviceImpl
+    {
+        private readonly HidDevice _device;
+
+        private bool _connected = true;
+
+        public override bool Connected => _connected;
+
+        public UwpHidDeviceInterface(IButtplugLogManager aLogManager, HidDevice aDevice)
+            : base(aLogManager)
+        {
+            _device = aDevice;
+            Name = _device.ToString();
+        }
+
+
+        public override void Disconnect()
+        {
+            _connected = false;
+            // noop?
+        }
+
+        public override async Task<ButtplugMessage> WriteValueAsync(uint aMsgId, byte[] aValue, bool aWriteWithResponse, CancellationToken aToken)
+        {
+            var report = _device.CreateOutputReport();
+            var w = new DataWriter();
+            w.WriteBytes(aValue);
+            report.Data = w.DetachBuffer();
+            await _device.SendOutputReportAsync(report);
+            return new Ok(aMsgId);
+        }
+
+        public override async Task<ButtplugMessage> WriteValueAsync(uint aMsgId, string aEndpointName, byte[] aValue, bool aWriteWithResponse,
+            CancellationToken aToken)
+        {
+            if (aEndpointName != Endpoints.Tx)
+            {
+                throw new ButtplugDeviceException(BpLogger, "UwpHidDevice doesn't support any write endpoint except the default.", aMsgId);
+            }
+
+            return await WriteValueAsync(aMsgId, aValue, aWriteWithResponse, aToken).ConfigureAwait(false);
+        }
+
+        public override Task<(ButtplugMessage, byte[])> ReadValueAsync(uint aMsgId, CancellationToken aToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<(ButtplugMessage, byte[])> ReadValueAsync(uint aMsgId, string aEndpointName, CancellationToken aToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SubscribeToUpdatesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task SubscribeToUpdatesAsync(string aEndpointName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Buttplug.Server.Managers.UwpHidManager/UwpHidManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+using Buttplug.Core.Logging;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Storage;
+using Buttplug.Devices.Configuration;
+
+namespace Buttplug.Server.Managers.UwpHidManager
+{
+    public class UwpHidManager : DeviceSubtypeManager
+    {
+        public static Guid HID_INTERFACE_CLASS_GUID = new Guid("4D1E55B2-F16F-11CF-88CB-001111000030");
+
+        public UwpHidManager(IButtplugLogManager aLogManager) : base(aLogManager)
+        {
+        }
+
+        protected async Task StartScanningInternal()
+        {
+            var factories = DeviceConfigurationManager.Manager.GetAllOfType<HIDProtocolConfiguration>();
+
+            var selectorPredicates = string.Empty;
+
+            foreach (var factory in factories)
+            {
+                if (selectorPredicates != string.Empty)
+                {
+                    selectorPredicates += " OR ";
+                }
+                var config = factory.Config as HIDProtocolConfiguration;
+                selectorPredicates += $"(System.DeviceInterface.Hid.VendorId:={config.VendorId}"
+                    + $" AND System.DeviceInterface.Hid.ProductId:={config.ProductId})";
+            }
+
+            // UWP doesn't have a 
+            var selector =
+                "System.Devices.InterfaceClassGuid:={" + HID_INTERFACE_CLASS_GUID + "}"
+                + " AND System.Devices.InterfaceEnabled:=System.StructuredQueryType.Boolean#True"
+                + $" AND {selectorPredicates}";
+            
+            var deviceList = await DeviceInformation.FindAllAsync(selector);
+            foreach (var device in deviceList)
+            {
+                var hidDevice =
+                    await HidDevice.FromIdAsync(device.Id,
+                        FileAccessMode.ReadWrite);
+                var vid = hidDevice.VendorId;
+                var pid = hidDevice.ProductId;
+                var finder = new HIDProtocolConfiguration(vid, pid);
+                var factory = DeviceConfigurationManager.Manager.Find(finder);
+                if (factory == null)
+                {
+                    // todo Error here
+                    continue;
+                }
+                var bpDevice = await factory.CreateDevice(LogManager, new UwpHidDeviceInterface(LogManager, hidDevice)).ConfigureAwait(false);
+                InvokeDeviceAdded(new DeviceAddedEventArgs(bpDevice));
+            }
+        }
+
+        public override void StartScanning()
+        {
+            StartScanningInternal().Wait();
+        }
+
+        public override void StopScanning()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override bool IsScanning()
+        {
+            return false;
+            //throw new NotImplementedException();
+        }
+    }
+}
