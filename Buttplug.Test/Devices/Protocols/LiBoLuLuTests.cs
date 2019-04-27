38a54592250d5143fdfcba57fecf9b066865cec7
blackspherefollower <darktech@black-sphere.co.uk>
2019-04-27 13:47:18 +0100

    feat: Adding support for Sistalk MonsterPub
    Turns out LiBo make these too, so only minor tweaks required.

--- Buttplug.Test/Devices/Protocols/LiBoLuLuTests.cs
+++ Buttplug.Test/Devices/Protocols/LiBoLuLuTests.cs
@@ -59,7 +59,7 @@ namespace Buttplug.Test.Devices.Protocols
             expected =
                 new List<(byte[], string)>()
                 {
-                    (new byte[] { 0 }, Endpoints.Tx),
+                    (new byte[] { 0 }, Endpoints.TxMode),
                 };
 
             await testUtil.TestDeviceMessage(new StopDeviceCmd(4), expected, false);
