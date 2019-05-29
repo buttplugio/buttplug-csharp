600f69ae1d5c03f73833d46b4b8a08405f704895
blackspherefollower <darktech@black-sphere.co.uk>
2019-05-29 17:46:53 +0100

    feat: Adding support for 5 more LiBo devices
    - Gugudai # Carlos - Suction Chicken
    - ShaYu # Shark - Enflating Rabbit
    - Yuyi # Lina - Leaf
    - LuWuShuang # Adel - Curved Rabbit
    - Libo # Lily - Double ended mini wand

--- Buttplug/Devices/Protocols/LiBoProtocol.cs
+++ Buttplug/Devices/Protocols/LiBoProtocol.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Buttplug.Core.Logging;
@@ -20,6 +21,7 @@ namespace Buttplug.Devices.Protocols
             public string Name;
             public uint VibeCount;
             public uint[] VibeOrder = new uint[] { 0, 1 };
+            public bool MultiCharacteristic = true;
         }
 
         internal static readonly Dictionary<string, LiBoType> DevInfos =
@@ -72,6 +74,47 @@ namespace Buttplug.Devices.Protocols
                     {
                         Name = "MonsterPub",
                         VibeCount = 1,
+                    }
+                },
+                {
+                    "Gugudai",
+                    new LiBoType()
+                    {
+                        Name = "Carlos",
+                        VibeCount = 2, // Suction as vibe
+                    }
+                },
+                {
+                    "ShaYu",
+                    new LiBoType()
+                    {
+                        Name = "Shark",
+                        VibeCount = 2,
+                        MultiCharacteristic = false,
+                    }
+                },
+                {
+                    "Yuyi",
+                    new LiBoType()
+                    {
+                        Name = "Lina",
+                        VibeCount = 1,
+                    }
+                },
+                {
+                    "LuWuShuang",
+                    new LiBoType()
+                    {
+                        Name = "Adel",
+                        VibeCount = 1,
+                    }
+                },
+                {
+                    "LiBo",
+                    new LiBoType()
+                    {
+                        Name = "Lily",
+                        VibeCount = 2,
                     }
                 },
             };
@@ -145,20 +188,43 @@ namespace Buttplug.Devices.Protocols
                 _vibratorSpeed[v.Index] = v.Speed;
             }
 
-            if (changed[_devInfo.VibeOrder[0]] || !SentVibration)
+            // Shark specific handling
+            if (_devInfo.VibeCount == 2 && !_devInfo.MultiCharacteristic)
             {
-                var ep = Endpoints.Tx;
-                if (_devInfo.VibeCount == 1 && (uint)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[0]] * 0x64) == 0)
+                if (!changed.Select(x => x).Any() && SentVibration)
                 {
-                    ep = Endpoints.TxMode;
+                    return new Ok(aMsg.Id);
                 }
 
+                SentVibration = true;
+                byte data = 0x00;
+                data |= (byte)((byte)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[0]] * 0x03) << 4);
+                data |= (byte)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[1]] * 0x03);
+                await Interface.WriteValueAsync(
+                    new[] { data },
+                    new ButtplugDeviceWriteOptions { Endpoint = Endpoints.Tx },
+                    aToken).ConfigureAwait(false);
+
+                return new Ok(aMsg.Id);
+            }
+
+            if (changed[_devInfo.VibeOrder[0]] || !SentVibration)
+            {
                 // Map a 0 - 100% value to a 0 - 0x64 value since 0 * x == 0 this will turn off the vibe if
                 // speed is 0.00
                 await Interface.WriteValueAsync(
                     new[] { Convert.ToByte((uint)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[0]] * 0x64)) },
-                    new ButtplugDeviceWriteOptions { Endpoint = ep },
+                    new ButtplugDeviceWriteOptions { Endpoint = Endpoints.Tx },
                     aToken).ConfigureAwait(false);
+
+                // Some devices don't really stop unless both characteristics are set to 0
+                if (_devInfo.VibeCount == 1 && (uint)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[0]] * 0x64) == 0)
+                {
+                    await Interface.WriteValueAsync(
+                        new[] { Convert.ToByte((uint)Math.Ceiling(_vibratorSpeed[_devInfo.VibeOrder[0]] * 0x64)) },
+                        new ButtplugDeviceWriteOptions { Endpoint = Endpoints.TxMode },
+                        aToken).ConfigureAwait(false);
+                }
             }
 
             if (_devInfo.VibeCount < 2 ||
