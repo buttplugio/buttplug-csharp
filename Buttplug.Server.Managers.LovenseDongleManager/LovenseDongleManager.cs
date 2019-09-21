5a036682dfcbdd11e8979e3b82792e459d776fb7
Kyle Machulis <kyle@machul.is>
2019-09-21 15:53:27 -0700

    chore: Update Copyright on Lovense Dongle Manager
    CLA signed, move to our copyright

--- Buttplug.Server.Managers.LovenseDongleManager/LovenseDongleManager.cs
+++ Buttplug.Server.Managers.LovenseDongleManager/LovenseDongleManager.cs
@@ -1,9 +1,8 @@
-﻿/*
- * Lovense Dongle Manager
- *
- *  Created on: 02 April 2019
- *      Author: Vasiliy Sukhoparov (VasiliSk)
- */
+﻿// <copyright file="LovenseDongleManager.cs" company="Nonpolynomial Labs LLC">
+// Buttplug C# Source Code File - Visit https://buttplug.io for more info about the project.
+// Copyright (c) Nonpolynomial Labs LLC. All rights reserved.
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root for full license information.
+// </copyright>
 
 using System;
 using System.Collections.Generic;
