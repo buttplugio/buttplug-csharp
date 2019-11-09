411080be77d69e47dd0e28558bcb82fc58d16e35
blackspherefollower <darktech@black-sphere.co.uk>
2019-11-09 16:03:14 +0000

    feat: Adding support for Lelo F1s
    This only adds output support: no sensor reading support yet.

---
+++ Buttplug/Devices/Protocols/LeloF1sProtocol.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="LeloF1sProtocol.cs" company="Nonpolynomial Labs LLC">
+// Buttplug C# Source Code File - Visit https://buttplug.io for more info about the project.
+// Copyright (c) Nonpolynomial Labs LLC. All rights reserved.
+// Licensed under the BSD 3-Clause license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Buttplug.Core.Logging;
+using Buttplug.Core.Messages;
+
+namespace Buttplug.Devices.Protocols
+{
+    internal class LeloF1sProtocol : ButtplugDeviceProtocol
+    {
+        private readonly double[] _vibratorSpeeds = { 0, 0 };
+
+        public LeloF1sProtocol(IButtplugLogManager aLogManager,
+                               IButtplugDeviceImpl aInterface)
+            : base(aLogManager,
+                   "Lelo F1s",
+                   aInterface)
+        {
+            AddMessageHandler<SingleMotorVibrateCmd>(HandleSingleMotorVibrateCmd);
+            AddMessageHandler<VibrateCmd>(HandleVibrateCmd, new MessageAttributes() { FeatureCount = 2 });
+            AddMessageHandler<StopDeviceCmd>(HandleStopDeviceCmd);
+        }
+
+        private async Task<ButtplugMessage> HandleStopDeviceCmd(ButtplugDeviceMessage aMsg, CancellationToken aToken)
+        {
+            BpLogger.Debug("Stopping Device " + Name);
+            return await HandleSingleMotorVibrateCmd(new SingleMotorVibrateCmd(aMsg.DeviceIndex, 0, aMsg.Id), aToken).ConfigureAwait(false);
+        }
+
+        private async Task<ButtplugMessage> HandleSingleMotorVibrateCmd(ButtplugDeviceMessage aMsg, CancellationToken aToken)
+        {
+            var cmdMsg = CheckMessageHandler<SingleMotorVibrateCmd>(aMsg);
+
+            return await HandleVibrateCmd(VibrateCmd.Create(cmdMsg.DeviceIndex, cmdMsg.Id, cmdMsg.Speed, 2), aToken).ConfigureAwait(false);
+        }
+
+        private async Task<ButtplugMessage> HandleVibrateCmd(ButtplugDeviceMessage aMsg, CancellationToken aToken)
+        {
+            var cmdMsg = CheckGenericMessageHandler<VibrateCmd>(aMsg, 2);
+            var changed = false;
+
+            foreach (var v in cmdMsg.Speeds)
+            {
+                if (Math.Abs(v.Speed - _vibratorSpeeds[v.Index]) < 0.001)
+                {
+                    continue;
+                }
+
+                changed = true;
+                _vibratorSpeeds[v.Index] = v.Speed;
+            }
+
+            if (!changed && SentVibration)
+            {
+                return new Ok(cmdMsg.Id);
+            }
+
+            SentVibration = true;
+
+            await Interface.WriteValueAsync(
+                new byte[]
+                {
+                    0x01,
+                    (byte)Convert.ToUInt32(_vibratorSpeeds[0] * 0x64),
+                    (byte)Convert.ToUInt32(_vibratorSpeeds[1] * 0x64),
+                }, aToken)
+                .ConfigureAwait(false);
+            return new Ok(aMsg.Id);
+        }
+    }
+}
